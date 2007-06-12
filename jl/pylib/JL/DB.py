6b91f67ee07bf1ec7c1087ba547c89e9b6480937
Ben Campbell <ben@scumways.com>
2007-06-12 11:37:25 +0000

    Hacky tweaks to accomdate postgresql upgrade on dev machine
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@30 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/pylib/JL/DB.py
+++ jl/pylib/JL/DB.py
@@ -7,13 +7,18 @@ import mysociety.config
 mysociety.config.set_file("../conf/general")
 
 def Connect():
+	# all a bit hacky...
 	u = mysociety.config.get('JL_DB_USER')
-	pwd = mysociety.config.get('JL_DB_PASS')
+	pwd = mysociety.config.get('JL_DB_PASS', 'UNSET' )
 	db = mysociety.config.get('JL_DB_NAME')
+	p = mysociety.config.get('JL_DB_PORT', 'UNSET')
 
-	if pwd == '':
-		conn = PgSQL.connect( user=u, database=db )
+	if pwd == 'UNSET':
+		# for dev machine
+		conn = PgSQL.connect( user=u, database=db, port=p )
 	else:
 		conn = PgSQL.connect( user=u, password=pwd, database=db )
+
+
 	return conn
 
