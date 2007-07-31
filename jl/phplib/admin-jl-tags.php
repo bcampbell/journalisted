14efb4326982f02f56ed8e5a46dd9c83c7185af1
Ben Campbell <ben@scumways.com>
2007-07-31 08:20:52 +0000

    tags blacklist interface working
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@76 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/phplib/admin-jl-tags.php
+++ jl/phplib/admin-jl-tags.php
@@ -6,8 +6,12 @@ require_once '../../phplib/utility.php';
 
 require_once '../phplib/misc.php';
 
-
-
+/*
+                db_query( "UPDATE article_tag t1 ".
+                    "SET freq=t1.freq+t2.freq ".
+                    "FROM article_tag t2 ".
+                    "WHERE t2.article_id=t1.article_id AND t2.tag=? and t1.tag=?");
+*/
 
 class ADMIN_PAGE_JL_TAGS {
     function ADMIN_PAGE_JL_TAGS() {
@@ -22,38 +26,45 @@ class ADMIN_PAGE_JL_TAGS {
 
     function display_blacklist() {
         print( "<h2>Tag blacklist</h2>\n" );
+        $newtag = strtolower( trim( get_http_var( 'newtag' ) ) );
+        $confirmed = get_http_var( 'confirmed' );
 
         $err = null;
-        if( get_http_var('addtag') == 'Add' ) {
-            /* submitted - validate input */
-            $newtag = trim( get_http_var( 'newtag' ) );
+        if( get_http_var('addtag') == 'Add Tag' ) {
+            // form submitted, check tag and request confirmation
+            $err = $this->check_newtag( $newtag );
+            if( !$err ) {
+                $cnt = db_getOne( "SELECT count(*) FROM article_tag WHERE tag=?", $newtag );
+                $backlink = sprintf( "?page=%s", urlencode( get_http_var('page') ) );
+?>
+<p>Adding '<?=$newtag;?>' to the blacklist will affect <?=$cnt;?> articles.</p>
+<form action="" method="get">
+<p>Are you sure you want to add this tag to the blacklist?</p>
+<input type="hidden" name="page" value="<?=get_http_var('page');?>" />
+<input type="hidden" name="newtag" value="<?=$newtag;?>"/>
+<input type="submit" name="addtag" value="Confirm" />
+<br>
+<br>
+<a href="<?=$backlink;?>">No - I've changed my mind!</a>
+</form>
 
-            if( $newtag == '' ) {
-                $err = "Tag is blank!";
-            } elseif( db_GetOne( "SELECT bannedtag FROM tag_blacklist " .
-                "WHERE bannedtag=?",
-                $newtag ) != null ) {
-                $err= sprintf( "'%s' is already in the blacklist!", $newtag );
+<?php
+                /* bail out early - don't need to display any more */
+                return;
             }
-
+        } elseif( get_http_var('addtag') == 'Confirm' ) {
+            $err = $this->check_newtag( $newtag );
             if( !$err ) {
                 db_query( "INSERT INTO tag_blacklist (bannedtag) VALUES (?)", $newtag );
-                // TODO: remove uses of the tag!
-
-
+                db_query( "DELETE FROM article_tag WHERE tag=?", $newtag );
                 db_commit();
-
-                print"<p>Added Tag</p>\n";
+                printf( "<p>Added '%s' to blacklist.</p>\n", $newtag);
             }
-            
-
-
         }
 
         if( $err ) {
             printf( "<p>ERROR: %s</p>\n", $err );
         }
-
 
         /* show existing tag blacklist */
         $blacklist = tag_get_blacklist();
@@ -62,25 +73,33 @@ class ADMIN_PAGE_JL_TAGS {
         foreach( $blacklist as $t ) {
             printf( "<tr><td>%s</td></tr>\n", $t );
         }
+        print( "</table>\n" );
 
 ?>
 <tr><td>
 <form action="" method="get">
 <input type="hidden" name="page" value="<?=get_http_var('page');?>" />
-<input type="text" name="newtag" />
-<input type="submit" name="addtag" value="Add" />
+<input type="text" name="newtag" value="<?=$newtag;?>"/>
+<input type="submit" name="addtag" value="Add Tag" />
 </form>
 </td></tr>
 <?php
-        print( "</table>\n" );
 
-
-/*
-        print "<pre>\n";
-        print_r( $_GET );
-        print "</pre>\n";
-*/
     }
+
+
+    function check_newtag( $newtag ) {
+        $err = null;
+        if( $newtag == '' ) {
+            $err = "Tag is blank!";
+        } elseif( db_GetOne( "SELECT bannedtag FROM tag_blacklist " .
+            "WHERE bannedtag=?",
+            $newtag ) != null ) {
+            $err= sprintf( "'%s' is already in the blacklist!", $newtag );
+        }
+        return $err;
+    }
+
 }
 
 
@@ -93,24 +112,6 @@ function tag_get_blacklist() {
             $blacklist[] = $r[0];
     }
     return $blacklist;
-}
-
-
-function update_blacklist( $values ) {
-    $blacklist = tag_get_blacklist();
-
-    $newlist = explode( "\n", $values['blacklist'] );
-    print "<pre>\n";
-    print_r( $newlist );
-    print "</pre>\n";
-
-    foreach( $newlist as $t ) {
-        $t=trim($t);
-        if(!$t)
-            continue;
-        if( in_array( $t, $blacklist ) ) {
-        }
-    }
 }
 
 
