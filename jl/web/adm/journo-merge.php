120c93964c7e6b462ae5e938fbd499e000c575cc
Ben Campbell <ben@scumways.com>
2007-11-05 10:14:22 +0000

    Added journo-merge tool to admin
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@158 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

---
+++ jl/web/adm/journo-merge.php
@@ -0,0 +1,176 @@
+<?php
+// sigh... stupid php include-path trainwreck...
+chdir( dirname(dirname(__FILE__)) );
+
+require_once '../conf/general';
+require_once '../phplib/page.php';
+require_once '../phplib/misc.php';
+require_once '../../phplib/db.php';
+
+?>
+<html>
+<head></head>
+<body>
+<h1>Merge Journo</h1>
+
+<?php
+
+
+$params = FormParamsFromHTTPVars();
+if( !$params['action'] )
+	EmitForm( $params );
+else
+{
+	$errs = ValidateForm( $params );
+	if( $errs )
+	{
+		EmitForm( $params, $errs );
+	}
+	else
+	{
+		if( $params['action'] == 'commit' )
+			MergeJourno( $params );
+		else
+			EmitPreview( $params );
+	}
+}
+
+?>
+</body>
+</html>
+
+<?php
+
+
+
+
+function FormParamsFromHTTPVars()
+{
+	$params = array();
+
+	$params['from_ref'] = get_http_var( 'from_ref', '' );
+	$params['into_ref'] = get_http_var( 'into_ref', '' );
+	$params['to_ref'] = get_http_var( 'to_ref', '' );
+	$params['action'] = get_http_var( 'action', '' );
+	return $params;
+}
+
+
+
+function EmitForm( $params, $errs=array() )
+{
+/*
+	print"<pre>\n";
+	print_r( $params );
+	print"</pre>\n";
+*/
+	if( $errs )
+	{
+		print( "<strong>ERRORS:</strong>\n<ul>\n" );
+		foreach( $errs as $e )
+		{
+			printf( "<li>%s</li>\n", $e );
+		}
+		print( "</ul>\n" );
+	}
+
+?>
+<form method="POST">
+Which journo do you want to merge? (and delete!)<br />
+<small>(use journo ref, eg 'fred-smiff')</small><br />
+<input type="text" name="from_ref" value="<?=$params['from_ref'];?>" /><br />
+<br />
+Which journo do you want to merge into?<br />
+<small>(eg 'fred-smith')</small><br />
+<input type="text" name="into_ref" value="<?=$params['into_ref'];?>" /><br />
+
+<input type="hidden" name="action" value="preview" />
+<input type="submit" value="preview" /><br />
+</form>
+<?php
+
+}
+
+
+function EmitPreview( $params )
+{
+?>
+<form method="POST">
+
+<p>OK, so you want to merge:</p>
+<?php JournoOverview( $params['from_ref'] ); ?>
+<p>into:</p>
+<?php JournoOverview( $params['into_ref'] ); ?>
+<p>is that right?</p>
+
+<input type="hidden" name="from_ref" value="<?=$params['from_ref'];?>" />
+<input type="hidden" name="into_ref" value="<?=$params['into_ref'];?>" />
+<input type="hidden" name="action" value="commit" />
+<input type="submit" value="MERGE THEM" /><br />
+</form>
+<?php
+}
+
+
+
+function ValidateForm( $params )
+{
+	$errs = array();
+	$fromj = db_getRow( "SELECT id,ref,prettyname,lastname,firstname FROM journo WHERE ref=?", $params['from_ref'] );
+	if( !$fromj )
+		$errs[] = sprintf( "Can't find FROM journo ('%s')", $params['from_ref'] );
+
+	$intoj = db_getRow( "SELECT id,ref,prettyname,lastname,firstname FROM journo WHERE ref=?", $params['into_ref'] );
+	if( !$intoj )
+		$errs[] = sprintf( "Can't find INTO journo ('%s')", $params['into_ref'] );
+
+	if( $params['from_ref'] == $params['into_ref'] )
+		$errs[] = "FROM and INTO journos can't be the same";
+
+	return $errs;
+}
+
+
+function MergeJourno( $params )
+{
+	$fromj = db_getRow( "SELECT id,ref,prettyname,lastname,firstname FROM journo WHERE ref=?", $params['from_ref'] );
+	$intoj = db_getRow( "SELECT id,ref,prettyname,lastname,firstname FROM journo WHERE ref=?", $params['into_ref'] );
+
+	$from_id = $fromj['id'];
+	$into_id = $intoj['id'];
+
+	db_do( "UPDATE journo_attr SET journo_id=? WHERE journo_id=?", $into_id, $from_id );
+	db_do( "UPDATE journo_alias SET journo_id=? WHERE journo_id=?", $into_id, $from_id );
+	db_do( "UPDATE journo_jobtitle SET journo_id=? WHERE journo_id=?", $into_id, $from_id );
+	db_do( "UPDATE journo_weblink SET journo_id=? WHERE journo_id=?", $into_id, $from_id );
+
+	db_do( "DELETE FROM journo WHERE id=?", $from_id );
+
+	db_commit();
+
+	printf("<p>Merged '%s' ('%s') into '%s' ('<a href=\"%s\">%s</a>')</p>\n",
+		$fromj['prettyname'], $fromj['ref'],
+		$intoj['prettyname'], '/' . $intoj['ref'], $intoj['ref'] );
+}
+
+function JournoOverview( $ref )
+{
+	$journo = db_getRow( "SELECT id,ref,prettyname FROM journo WHERE ref=?", $ref );
+
+	$r = db_query( "SELECT a.srcorg as orgid, COUNT(*) as numarticles ".
+		"FROM (article a INNER JOIN journo_attr attr ON a.id=attr.article_id) ".
+		"WHERE attr.journo_id=? ".
+		"GROUP BY a.srcorg",
+		$journo['id'] );
+	printf( "'%s' (id=%s ref='%s')\n", $journo['prettyname'], $journo['id'], $journo['ref'] );
+	print( "<table border=1>\n" );
+	print( "<tr><th>Outlet</th><th>Num articles</th></tr>\n" );
+	$orgs = get_org_names();
+	while( $row = db_fetch_array( $r ) )
+	{
+		$orgid = $row['orgid'];
+		printf("<tr><td>%s</td><td>%d</td></tr>\n", $orgs[$orgid], $row['numarticles'] );
+	}
+	print( "</table>\n" );
+}
+
