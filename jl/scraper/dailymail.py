cdc1db235d0bc79e11accf3bc1cd08af371feb20
Ben Campbell <ben@scumways.com>
2007-11-06 13:02:13 +0000

    Added loads more RSS feeds
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@161 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/scraper/dailymail.py
+++ jl/scraper/dailymail.py
@@ -4,6 +4,8 @@
 # Licensed under the Affero General Public License
 # (http://www.affero.org/oagpl.html)
 #
+# TODO:
+# - columnists require separate scrape path (no rss feeds!)?
 #
 
 import re
@@ -16,13 +18,67 @@ from JL import ArticleDB,ukmedia
 
 
 rssfeeds = {
+	'Homepage': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1766',
+	'Mail on Sunday': 'http://feeds.feedburner.com/dailymail/MailonSundayHomepage',
 	'News': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1770',
-	'World News': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1770',
+	'News headlines': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1770&in_chn_id=1674&in_headlines=Y',
+	'World news': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1811',
+	# Comment has editorials, but no columnists...
+	'Comment': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1787',
+	'Sport': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1771',
+	'Football': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1779',
+	'Rugby union': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1780',
+	'Cricket': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1849',
+	'Other sport': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1781',
+	'TV & showbiz': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1773',
+	'Health': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1774',
+	'Diet & fitness': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1798',
+	'Women & family': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1799',
+	'Femail': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1879',
+	'In the stands': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1867',
+	# promos have missing dates... (maybe other stuff missing too)...
+#	'Promotions': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1777',
+	'Live Night & Day': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1889',
+	'Health notes': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1909',
+	'Baz Bamigboye': 'http://feeds.feedburner.com/dailymail/BazBamigboye',
+	'Jaci Stephen': 'http://feeds.feedburner.com/dailymail/JaciStephen',
+	'Reviews': 'http://feeds.feedburner.com/dailymail/TVShowbizReviews',
+	'Rugby league': 'http://feeds.feedburner.com/dailymail/MailOnlineRugbyLeague',
+	'Motorsport': 'http://feeds.feedburner.com/dailymail/Motorsport',
+	'Golf': 'http://feeds.feedburner.com/dailymail/MailOnlineGolf',
+	'American sports': 'http://feeds.feedburner.com/dailymail/AmericanSports',
+	'Schools football': 'http://feeds.feedburner.com/dailymail/SchoolsFootball',
+	'Science and technology': 'http://feeds.feedburner.com/dailymail/ScienceandTech',
+	'Horse racing': 'http://feeds.feedburner.com/dailymail/HorseRacing',
+	'Tennis': 'http://feeds.feedburner.com/dailymail/tennis',
+	'Big Brother': 'http://feeds.feedburner.com/dailymail/BigBrother',
+	'Cricket World Cup': 'http://feeds.feedburner.com/TheMailOnline/Cricketworldcup',
+	'Arsenal': 'http://feeds.feedburner.com/dailymail/arsenal',
+	'Aston Villa': 'http://feeds.feedburner.com/dailymail/astonvilla',
+	'Birmingham': 'http://feeds.feedburner.com/dailymail/birmingham',
+	'Blackburn': 'http://feeds.feedburner.com/dailymail/blackburn',
+	'Bolton': 'http://feeds.feedburner.com/dailymail/bolton',
+	'Chelsea': 'http://feeds.feedburner.com/dailymail/chelsea',
+	'Derby': 'http://feeds.feedburner.com/dailymail/derby',
+	'Everton': 'http://feeds.feedburner.com/dailymail/everton',
+	'Fulham': 'http://feeds.feedburner.com/dailymail/fulham',
+	'Liverpool': 'http://feeds.feedburner.com/dailymail/liverpool',
+	'Manchester City': 'http://feeds.feedburner.com/dailymail/mancity',
+	'Manchester United': 'http://feeds.feedburner.com/dailymail/manutd',
+	'Middlesbrough': 'http://feeds.feedburner.com/dailymail/middlesbrough',
+	'Newcastle': 'http://feeds.feedburner.com/dailymail/newcastle',
+	'Portsmouth': 'http://feeds.feedburner.com/dailymail/portsmouth',
+	'Reading': 'http://feeds.feedburner.com/dailymail/reading',
+	'Sunderland': 'http://feeds.feedburner.com/dailymail/sunderland',
+	'Tottenham': 'http://feeds.feedburner.com/dailymail/tottenham',
+	'West Ham': 'http://feeds.feedburner.com/dailymail/westham',
+	'Wigan': 'http://feeds.feedburner.com/dailymail/wigan',
+	'Rangers': 'http://feeds.feedburner.com/dailymail/rangers',
+	'Celtic': 'http://feeds.feedburner.com/dailymail/celtic',
+}
 
 
-#	'Homepage':'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1766',
-#	'Comment': 'http://www.dailymail.co.uk/pages/xml/index.html?in_page_id=1787' 
-	}
+
 
 
 
@@ -52,7 +108,7 @@ def Extract( html, context ):
 	# get Description
 	foo = soup.find( 'meta', {'name':'description'} )
 	art['description'] = foo[ 'content' ]
-	art['description'] = ukmedia.DescapeHTML( art['description'] )
+	art['description'] = ukmedia.FromHTML( art['description'] )
 
 	articlediv = soup.find( 'div', id='ArtContent' )
 
@@ -144,8 +200,12 @@ urltrimpat=re.compile( "(.*?[?]in_article_id=[0-9]+).*$" )
 def ScrubFunc( context, entry ):
 	"""mungefunc for ukmedia.FindArticlesFromRSS()"""
 
-	# dailymail RSS feeds go through feedburner, but luckily the original url is still there...
-	url = entry.feedburner_origlink
+	# most dailymail RSS feeds go through feedburner, but luckily the original url is still there...
+	url = context[ 'srcurl' ]
+	if url.find('feedburner') != -1:
+		url = entry.feedburner_origlink
+
+	# trim off cruft
 	url = urltrimpat.sub( "\\1", url )
 
 	context['srcurl'] = url
