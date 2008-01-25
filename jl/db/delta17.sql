d3816eef6bd75bd201cb11b215228ff1457560d1
Ben Campbell <ben@scumways.com>
2008-01-25 12:49:38 +0000

    db: some changes already applied, new change to add an index to the article title
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@222 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/db/delta17.sql
+++ jl/db/delta17.sql
@@ -1,3 +1,4 @@
--- add normalised name for fuzzy matching
-ALTER TABLE journo ADD COLUMN normalisedname text NOT NULL DEFAULT '';
+-- index the article title column to speed up duplicate handling
+-- (and maybe other stuff too)
+CREATE INDEX article_title_idx ON article(title)
 
