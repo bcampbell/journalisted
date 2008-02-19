0ee75466a94edb641274653028ee97fe1cf582e2
Ben Campbell <ben@scumways.com>
2008-02-19 09:18:28 +0000

    added scotland-on-sunday feeds
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@247 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/hacks/scotsman-scrape-rsslist.py
+++ jl/hacks/scotsman-scrape-rsslist.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python2.4
 #
-# get a list of RSS feeds from The Scotsman
+# get a list of RSS feeds from The Scotsman and Scotland on Sunday
 #
+# TODO: generalise to all the Johnston Group Newspapers!
 
 import re
 import sys
@@ -13,21 +14,28 @@ from BeautifulSoup import BeautifulSoup
 from JL import ukmedia
 
 
-masterpage = "http://thescotsman.scotsman.com/webfeeds.aspx"
-feedpat = re.compile( "getFeed[.]aspx[?]Format=rss&sectionid=[0-9]+" )
 
+def DoIt( masterpage ):
+	feedpat = re.compile( "getFeed[.]aspx[?]Format=rss&sectionid=[0-9]+" )
+	f = urllib2.urlopen( masterpage )
+	html = f.read()
+	f.close()
 
-f = urllib2.urlopen( masterpage )
-html = f.read()
-f.close()
+	soup = BeautifulSoup( html )
 
-soup = BeautifulSoup( html )
+	for a in soup.findAll('a', href=feedpat):
+		name = ukmedia.FromHTML(a.string)
+		url = urlparse.urljoin( masterpage, a['href'] )
+		print "\t\"%s\": \"%s\"," %( name,url )
+
 
 print "scotsman_rssfeeds = {"
-for a in soup.findAll('a', href=feedpat):
-	name = ukmedia.FromHTML(a.string)
-	url = urlparse.urljoin( masterpage, a['href'] )
-	print "\t\"%s\": \"%s\"," %( name,url )
+DoIt( "http://thescotsman.scotsman.com/webfeeds.aspx" )
+print "}"
+
+
+print "scotlandonsunday_rssfeeds = {"
+DoIt( "http://scotlandonsunday.scotsman.com/webfeeds.aspx" )
 print "}"
 
 
