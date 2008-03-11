a8bf63248dd7729c6f628b86287ad6a11d0ed289
Ben Campbell <ben@scumways.com>
2008-03-11 09:24:59 +0000

    Common interface to unify news outlet scrapers
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@292 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

---
+++ jl/scraper/scrapefront.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2.4
+#
+# front end to present a single interface for using the
+# news outlet scrapers.
+# CalcSrcID(url) - 
+#
+# TODO:
+#
+# - alter blogs.py to work with this (or move blog
+#   functionality out into the other scrapers)
+#
+# - add interface to scrape a single url
+#   a) figure out which scraper should handle it (use CalcSrcID())
+#   b) context = scraper.ContextFromURL( url )
+#   c) fetch the page
+#   d) scraper.Extract( html, context)
+#   This stuff is already supported by the scrapers.
+#
+
+scrapers = ( 'bbcnews', 'dailymail', 'express', 'ft',
+	'guardian', 'herald', 'independent', 'mirror',
+	'scotsman', 'sun', 'telegraph', 'times' )
+
+modules = []
+for s in scrapers:
+	modules.append( __import__( s ) )
+
+def CalcSrcID( url ):
+	"""determine a unique srcid from the url. returns None if no scrapers handle this url"""
+	for m in modules:
+		srcid = m.CalcSrcID( url )
+		if srcid:
+			return srcid
+
+	# if we get this far, we can't determine srcid.
+	# actually, blogs.py might be able to handle this url,
+	# but that isn't integrated yet...
+	return None
+
+
