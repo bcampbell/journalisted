7c06f0093557008b5272a5510947e37728ec407f
Tom Lynn <tom.lynn@dummyaddress.com>
2008-03-18 22:09:45 +0000

    Turned wikipedia.py into a behemoth. It seems to be working well now though.
    It slices and dices the page, and only keeps relatively pertinent headings.
    Throws away cross-references within the text, that style may be suitable for
    wikipedia, but probably not for us. It also truncates long lists of things
    (bibliographies being the main offenders).
    
    It throws an exception if it's worried it may have the wrong person
    (no sign they're a journalist).
    
    Linking it to the database more sensibly is still TODO.
    
    
    
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@319 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/scraper/wikipedia.py
+++ jl/scraper/wikipedia.py
@@ -9,15 +9,24 @@
 Scrapes wikipedia articles (headings and paragraphs).
 '''
 
+###
 # Doubtless I'll find that there's a pre-XML-ified version of wikipedia
 # somewhere, but until then...
+#
+# ...Yes there is, see <http://en.wikipedia.org/wiki/Help:Export>.
+# However,
+#    "In the current version the export format does not contain
+#     an XML replacement of wiki markup",
+# so it's nearly useless. It might be worth trying http://pywikipediabot.sf.net
+# but it's not in either Debian or the Cheeseshop, so it's a nasty dependency.
+###
 
 import sys
 import re
 import urllib2
 
 sys.path.append('../pylib')
-from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulSoup, NavigableString, Tag
 from JL import DB, ukmedia, ScraperUtils
 from datetime import datetime
 
@@ -37,8 +46,48 @@ def Extract(html, context):
     if 'Wikipedia does not have an article with this exact name' in html:
         raise Exception("No article with this exact name.")
 
-    if 'href="/wiki/Journalist"' not in html:
-        raise Exception("No link to 'Journalist' page found, may be wrong person.")
+    if 'Wikipedia:Disambiguation' in html:
+        raise Exception("Disambiguation page")
+
+    OCCUPATIONS = [
+        'Journalist', 'Journalism', 'Science_journalist', 'Author', 'Writer',
+        'Novelist',
+        'Member_of_Parliament',
+        'Broadcasting', 'Broadcaster'  # a bit chancy, but excusable
+        
+        # should we assume sports are sufficient indicators too?
+        # there is, after all, only one wikipedia page for all of these names
+    ]
+    # Check for a definition list at the start
+    tables_re = re.compile(r'<table(?=\W).*?</table>', flags=re.DOTALL)
+    html2 = '\n'.join(tables_re.split(html))
+    tag = re.search(r'<(dd|p)(?=\W)', html2)
+    if tag.group(1)=='dd':
+        # Check for "For ..., see ...".
+        dd = re.search(r'<dd.*?>(.*?)</dd', html2, flags=re.DOTALL).group(1)
+        raise Exception("Unsure if this is the right person")
+        # This test isn't picky enough:
+        #
+        #for job in OCCUPATIONS:
+        #    if job.lower() in dd.lower():
+        #        raise Exception("Probably the wrong page")
+        #print "WARNING: possibly the wrong page:"
+        #print "    %s" % dd.lstrip().encode('utf-8')
+
+    # Any page which uses the word 'journalist' is assumed to be the right one.
+    # People related to journalists may fall foul of this, but they're usually
+    # journos too :-).  We're careful to miss "see Martin Arnold (journalist)".
+    
+    if ' journalist' not in html and 'Journalist ' not in html:
+        # FIXME: This catches problem pages (inc. disambiguation pages) fine,
+        # but we should have a way to force scraping the page.
+        
+        ok = False
+        for word in OCCUPATIONS:
+            if ('href="/wiki/%s"' % word) in html:
+                ok = True
+        if not ok:
+            raise Exception("No link to writing-occupation page found, may be wrong person.")
 
     m = re.search(r'<li id="lastmod"> This page was last modified on (.*?, at \d\d:\d\d).</li>', html)
     if m is None:
@@ -57,7 +106,18 @@ def Extract(html, context):
     # Remove relative links and [<number>] links.
     for a in soup.findAll('a'):
         if a.string and re.match(r'\[\d+\]', a.string):
-            a.extract()
+            extract_li = False
+            if a.parent and a.parent.name == 'li':
+                for sub in a.parent.contents:
+                    if isinstance(sub, Tag):
+                        if sub is not a: break
+                        extract_li = True  # a is the first subtags
+                    else:
+                        if sub.strip() not in ('', '^'): break
+            if extract_li:
+                a.parent.extract()
+            else:
+                a.extract()
         elif not a.get('href', '').startswith('http://'):
             a.replaceWith(a.string or '')
 
@@ -68,29 +128,124 @@ def Extract(html, context):
         else:
             span.replaceWith(span.string or '')
     
-    # Remove references
+    # Remove references in the text
     for sup in soup.findAll('sup', attrs={'class': 'reference'}):
         sup.extract()
+
+    # Remove references at the bottom (not reliable)
+    for sup in soup.findAll('li', attrs={'id': re.compile('_note.*')}):
+        sup.extract()
     
-    tags = soup.body.findAll(['h2', 'p'])
+    # Replace their ridiculous use of tables for pull-quotes with blockquotes
+    for tag in soup.findAll('table'):
+        if dict(tag.attrs).get('class')=='cquote':
+            celltext = tag.findAll('td', limit=2)[1].renderContents(None)
+            quote = Tag(soup, u'blockquote')
+            quote.append(celltext)
+            tag.replaceWith(quote)
+    
+    tags = soup.body.findAll(['h2', 'p', 'ul', 'ol', 'blockquote'])
+    
+    # blockquotes cause problems - they contain other tags.
+    # Remove all tags with a blockquote parent from the list of tags
+    # so that we don't break the parent relationships when assigning
+    # tags to headings.
+    
+    for tag in list(tags):
+        for parent in get_parents(tag):
+            if parent.name=='blockquote':
+                tags.remove(tag)
 
-    def strip(s):
-        '''Strip outer whitespace from s, including <br /> tags.'''
-        s = re.search(r'(?s)\s*(?:<br\s*/>\s*)*(.*)', s).group(1)
-        return re.search(r'(?s)(.*?)\s*(?:<br\s*/>\s*)*\Z', s).group(1)
+    # Drop the junk after the article (not always present)
+    for i, tag in enumerate(tags):
+        if tag.name=='ul' and tag.findAll('li', id='ca-nstab-main'):
+            break
+    tags = tags[:i]
 
-    # Remove paragraphs with no strings and headings with no paragraphs.
-    i = 1
-    while i < len(tags):
-        if tags[i].name=='h2' and tags[i-1].name=='h2':
-            tags.pop(i-1)
-        elif tags[i].name=='p' and not strip(tags[i].renderContents(None)):
-            tags.pop(i)
+    # Build a list of headings with their sub-tags.
+    headings = [(None, [])]
+    for tag in tags:
+        if tag.name=='h2':
+            headings.append((tag, []))
         else:
-            i += 1
+            headings[-1][1].append(tag)
+
+    def drop_heading(heading):
+        print 'DROPPING:', tagtext(heading)
+        return None
     
-    while tags and tags[-1].name=='h2':
-        tags.pop()
+    def fix_heading(heading, tags):
+        '''
+        Remove paragraphs with no strings.
+        Remove non-special headings that don't start with a paragraph.
+        Remove lists from non-special headings.
+        '''
+        SPECIAL = ['Books', 'Works', 'Bibliography', 'External links']
+        tags = [tag for tag in tags if tag is not None and
+                    tag.name!='p' or tag.renderContents(None).strip()]
+        special = False
+        heading_text = tagtext(heading)
+        for word in SPECIAL:
+            if word.lower() in heading_text.lower():
+                special = True
+        if heading_text == 'External links and references':
+            set_heading_text(heading, 'External links')
+        # Shorten lists (even special ones).
+        # The motivation is that some pages like to list reams of crap,
+        # usually in bibliographies, but in other things too.
+        found_lis = 0
+        MAX_ITEMS = 10  # per headed section
+        for tag in list(tags):
+            if tag.name in ('ul', 'ol'):
+                for li in tag.findAll('li', recursive=False):
+                    found_lis += 1
+                    if found_lis > MAX_ITEMS:
+                        li.extract()
+        # Remove any now-empty uls and ols.
+        # Harder than it sounds, due to nested lists.
+        temp = Tag(soup, 'p')
+        for tag in tags:
+            temp.append(tag)
+        for tag in temp.findAll(('ul', 'ol')):
+            if not tag.findAll(('ul', 'ol', 'li')):
+                tag.extract()
+        tags = temp.contents
+        if found_lis > MAX_ITEMS:
+            # Add " (some omitted)" to heading
+            if heading_text:
+                heading_text = heading_text.replace(' (incomplete)', '')
+                if context['srcurl'].startswith('http:'):
+                    heading_text += ' (some <a href="%s">omitted</a>)' % context['srcurl']
+                else:
+                    heading_text += ' (some omitted)'  # no "relative" links
+                set_heading_text(heading, heading_text)
+        if not special:
+            if heading is not None:
+                # Remove non-special headings which don't start with a paragraph.
+                if not tags or tags[0].name != 'p':
+                    return drop_heading(heading)
+                # Remove non-special headings containing lists.
+                for tag in tags:
+                    if tag.name in ('ul', 'ol'):
+                        return drop_heading(heading)
+            else:
+                # Remove lists from None (before first heading, if any).
+                tags = [tag for tag in tags if tag.name not in ('ul', 'ol')]
+        return (heading, tags)
+    
+    headings = [fix_heading(*ht) for ht in headings]
+    headings = [ht for ht in headings if ht is not None]
+    
+    # Flatten.
+    tags = []
+    for heading, htags in headings:
+        if heading is not None:
+            tags.append(heading)
+        if tagtext(heading)=='Death':
+            for tag in htags:
+                for year in re.findall(r'19[0-9]\d(?!\d)', tag.renderContents(None)):
+                    raise Exception("ex-person! Died in %s!" % year)
+        tags += htags
     
     # Replace contents heading with an empty paragraph tag.
     # Since it's now the only one, we can use it to detect the
@@ -99,7 +254,7 @@ def Extract(html, context):
     # Also strip tag contents and stringify.
     
     for i, tag in enumerate(tags):
-        if tag.name=='h2' and tag.string=='Contents':
+        if tag.name=='h2' and tagtext(tag)=='Contents':
             tags[i] = u'<p></p>'
         else:
             tagname = unicode(tag.name)
@@ -113,6 +268,35 @@ def Extract(html, context):
     
     context['content'] = text
     return context
+
+
+## Helpers
+
+def get_parents(tag):
+    parents = []
+    while tag:
+        tag = tag.parent
+        if tag: parents.append(tag)
+    return parents
+
+def set_heading_text(heading, text):
+    while heading.contents:
+        heading.contents[-1].extract()
+    heading.append(NavigableString(text))
+
+def strip(s):
+    '''Strip outer whitespace from s, including <br /> tags.'''
+    s = re.search(r'(?s)\s*(?:<br\s*/>\s*)*(.*)', s).group(1)
+    return re.search(r'(?s)(.*?)\s*(?:<br\s*/>\s*)*\Z', s).group(1)
+
+def tagtext(tag, strip=True):  # much safer than tag.string
+    if tag is None:
+        return u''
+    text = tag.renderContents(None)
+    if strip:
+        text = text.strip()
+    return text
+
 
 
 def FindArticles():
@@ -132,6 +316,7 @@ def ContextFromURL(url):
     """Build up an article scrape context from a bare url."""
     context = {}
     prettyname = url.split('/')[-1].replace('_', ' ')
+    prettyname = prettyname.decode('utf-8') # just in case
     context['title'] = u'wikipedia: ' + prettyname
     context['description'] = u'Wikipedia article on ' + prettyname
     context['srcurl'] = url
@@ -144,8 +329,38 @@ def ContextFromURL(url):
 
 
 if __name__=='__main__':
-    ScraperUtils.RunMain(FindArticles, ContextFromURL, Extract)
+    if 1:
+        for i, arg in enumerate(sys.argv):
+            if not arg.startswith('-') and not arg.startswith('http'):
+                sys.argv[i] = 'file:' + arg
+        ScraperUtils.RunMain(FindArticles, ContextFromURL, Extract)
+    else:
+        # TEST CODE
+        import os, sys
+        for fname in os.listdir('wikip2'):
+            if fname.startswith('_'): continue  # "no article with this" etc.
+            import StringIO
+            sys.argv[1:] = ['-d', '-u', 'file:wikip2/' + fname]
+            print '%s: ' % fname,
+            sys.stderr = StringIO.StringIO()
+            import pdb
+            try:
+                x = ScraperUtils.RunMain(FindArticles, ContextFromURL, Extract)
+            except Exception, e:
+                pdb.set_trace()
+            bad = False
+            for line in StringIO.StringIO(sys.stderr.getvalue()):
+                if line.startswith('Exception:'):
+                    if 'No article with this exact name' in line: bad = True
+                    sys.stdout.write(line)
+            if bad:
+                print '(Renaming as _%s)' % fname
+                os.rename('wikip2/'+fname, 'wikip2/_'+fname)
+                assert os.path.isfile('wikip2/_'+fname)
+            sys.stderr = sys.__stderr__
     if 0:
+        # This might be useful for building a non-ScraperUtils alternative,
+        # so I'm keeping it here for the time being.
         import pprint
         args = sys.argv[1:]
         if '--help' in args or '-h' in args:
