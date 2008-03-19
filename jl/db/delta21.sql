1ef61d0664aa50dd05766025fb35162ed48a502e
Ben Campbell <ben@scumways.com>
2008-03-19 21:23:51 +0000

    Beating the technorati bloglink scraper into bloody, bruised shape.
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@320 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/db/delta21.sql
+++ jl/db/delta21.sql
@@ -2,12 +2,13 @@
 
 CREATE TABLE article_bloglink (
 	id serial NOT NULL PRIMARY KEY,
-	nearestpermalink text NOT NULL,
+	nearestpermalink text NOT NULL default '',
+	title text NOT NULL default '',
 	blogname text NOT NULL,
 	blogurl text NOT NULL,
 	linkurl text NOT NULL,
 	linkcreated timestamp NOT NULL,
-	excerpt text NOT NULL,
+	excerpt text NOT NULL default '',
 	via text NOT NULL default '',
 	article_id integer REFERENCES article(id) NOT NULL
 );
