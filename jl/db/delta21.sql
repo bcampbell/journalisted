a61f9991cee59f38aef1b6aa961237e12b35b7e2
Ben Campbell <ben@scumways.com>
2008-03-28 11:38:05 +0000

    Tidying up rough edges on the bloglink/commentlink gathering system
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@329 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/db/delta21.sql
+++ jl/db/delta21.sql
@@ -2,14 +2,14 @@
 
 CREATE TABLE article_bloglink (
 	id serial NOT NULL PRIMARY KEY,
+	article_id integer REFERENCES article(id) NOT NULL,
 	nearestpermalink text NOT NULL default '',
 	title text NOT NULL default '',
 	blogname text NOT NULL,
 	blogurl text NOT NULL,
-	linkurl text NOT NULL,
+--	linkurl text NOT NULL,
 	linkcreated timestamp NOT NULL,
 	excerpt text NOT NULL default '',
-	via text NOT NULL default '',
-	article_id integer REFERENCES article(id) NOT NULL
+	source text NOT NULL default ''
 );
 
