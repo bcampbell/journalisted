8757dee17b3919fd8d053c3b7cbd9fcf7e6a7110
Tom Lynn <tom.lynn@dummyaddress.com>
2008-04-08 17:13:11 +0000

    Reading journo_id in email.
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@383 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/web/journo.php
+++ jl/web/journo.php
@@ -565,7 +565,7 @@ function emit_wikipedia_bio( $journo )
 
 function emit_journo_mailto( $journo )
 {
-	$row = db_getRow("SELECT email, srcurl FROM journo_email WHERE journo_ref=?", $journo['ref']);
+	$row = db_getRow("SELECT email, srcurl FROM journo_email WHERE journo_id=?", $journo['id']);
 	if ($row)
 	{
     	$shorturl = $row['srcurl'];
