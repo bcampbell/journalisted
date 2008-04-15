26ec404946b3ba0c9ecef02feb9e66deabad5309
Tom Lynn <tom.lynn@dummyaddress.com>
2008-04-15 17:51:11 +0000

    Added a scraper for individual Times journalist article lists.
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@416 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/pylib/JL/ScraperUtils.py
+++ jl/pylib/JL/ScraperUtils.py
@@ -8,20 +8,28 @@ import sys
 import ukmedia, ArticleDB
 
 
-def RunMain( findarticles_fn, contextfromurl_fn, extract_fn, post_fn=None, maxerrors=20  ):
+def RunMain( findarticles_fn, contextfromurl_fn, extract_fn, post_fn=None, maxerrors=20,
+	         prepare_parser_fn=None, after_parsing_fn=None ):
 	"""A generic(ish) main function that all scrapers can use
 
 	Scrapers pass in callbacks:
 	findarticles_fn: return a list of article contexts for a full scrape
 	contextfromurl_fn: create an article context from a bare url
 	extract_fn: function to process an HTML page and return an article
+	prepare_parser_fn(parser): function that adds any additional options to parser
+	after_parsing_fn(options, args): function that returns adjusted (options, args).
 	"""
 
 	parser = OptionParser()
 	parser.add_option( "-u", "--url", dest="url", help="scrape a single article from URL", metavar="URL" )
 	parser.add_option("-d", "--dryrun", action="store_true", dest="dryrun", help="don't touch the database")
 
+	if prepare_parser_fn:
+		prepare_parser_fn(parser)
+	
 	(options, args) = parser.parse_args()
+	if after_parsing_fn:
+		(options, args) = after_parsing_fn(options, args)
 
 	found = []
 	if options.url:
