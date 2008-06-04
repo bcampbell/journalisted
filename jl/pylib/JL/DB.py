8bff88557a117ca0512aad43211fc1a72c5ba6da
Ben Campbell <ben@scumways.com>
2008-06-04 14:41:52 +0000

    python database bindings switched over to psycopg2
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@492 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/pylib/JL/DB.py
+++ jl/pylib/JL/DB.py
@@ -1,7 +1,48 @@
 import sys
 sys.path.append("../../pylib")
 
-from pyPgSQL import PgSQL
+#from pyPgSQL import PgSQL
+import psycopg2
+import psycopg2.extras as pe
+
+class PgSQL:
+    '''
+    A PgSQL-compatible wrapper over psycopg2.
+    The only difference is that PgSQL supports cursor.execute(query, nonseq).
+    '''
+    @staticmethod
+    def connect(**kwargs):
+        return FakeConn(psycopg2.connect(**kwargs))
+
+class FakeConn(object):
+    def __init__(self, conn):
+        self.conn = conn
+    def cursor(self):
+        return FakeCursor(self.conn.cursor(cursor_factory=pe.DictCursor))
+    def commit(self):
+        return self.cursor().execute('COMMIT')
+    def rollback(self):
+        return self.cursor().execute('ROLLBACK')
+
+
+class FakeCursor(object):
+	def __init__(self, cur):
+		self.cur = cur
+	def __getattr__(self, name):
+		if name in ('execute', 'commit', 'rollback'):
+			return lambda *args, **kwargs: getattr(FakeCursor, name)(self, *args, **kwargs)
+		method = getattr(self.cur, name)
+		return lambda *args, **kwargs: method(*args, **kwargs)
+
+	def execute(self, query, *args, **kwargs):
+		if len(args)!=1 or not isinstance(args[0], (dict, list, tuple)):
+			args = [args]
+		return self.cur.execute(query, *args, **kwargs)
+	def commit(self):
+		return self.cur.execute('COMMIT')
+	def rollback(self):
+		return self.cur.execute('ROLLBACK')
+
 
 import mysociety.config
 mysociety.config.set_file("../conf/general")
