ec729e16d856ffc7ac0df9aed0e6ec01a39e85cc
Ben Campbell <ben@scumways.com>
2008-06-26 10:57:53 +0000

    search page: Pagination
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@504 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/web/search.php
+++ jl/web/search.php
@@ -7,10 +7,13 @@ require_once '../phplib/cache.php';
 require_once '../phplib/misc.php';
 require_once '../../phplib/db.php';
 
+/* hmm... not happy about this absolute path here... */
 include "/usr/share/php5/xapian.php";
 
 $DBPATH = OPTION_JL_XAPDB;
 
+# IDs for extra values tied to articles in the xapian db
+# NOTE: these need to be kept in sync with bin/indexer! (written in python)
 define( 'XAP_ARTICLE_ID', 0 );
 define( 'XAP_TITLE_ID', 1 );
 define( 'XAP_PUBDATE_ID', 2 );
@@ -18,14 +21,41 @@ define( 'XAP_SRCORG_ID', 3 );
 define( 'XAP_PERMALINK_ID', 4 );
 define( 'XAP_JOURNOS_ID', 5 );
 
+define( 'DEFAULT_NUM_PER_PAGE', 100 );
+
 page_header( "" );
 
-$kw = get_http_var( 'kw', '' );
+$query = get_http_var( 'q', '' );
+$num_per_page = (int)get_http_var('num', DEFAULT_NUM_PER_PAGE );
+$start = (int)get_http_var('start', '0' );
+
 
 ?>
 
 <div id="maincolumn">
 <h2>Full text search</h2>
+
+<?php
+
+EmitQueryForm( $query, $num_per_page );
+
+if( $query )
+{
+    DoQuery( $query, $start, $num_per_page );
+}
+
+?>
+</div>
+<?php
+
+page_footer();
+
+
+
+function EmitQueryForm( $query, $num_per_page )
+{
+
+?>
 <small>
 <p>examples:<br/>
 <code>hash browns</code> - (articles with hash OR brown)<br/>
@@ -39,26 +69,18 @@ Detailed query syntax <a href="http://www.xapian.org/docs/queryparser.html">here
 </small>
 
 <form method="get">
-Search for: <input type="text" name="kw" value="<?php echo htmlspecialchars($kw); ?>" />
+Search for: <input type="text" name="q" value="<?php echo htmlspecialchars($query); ?>" />
 
 <input type="submit" value="go" />
 </form>
 
 <?php
 
-if( $kw )
-{
-    Doit( $kw );
 }
 
-?>
-</div>
-<?php
-
-page_footer();
 
 
-
+/* helper for formatting results */
 function PostedFragment( &$r )
 {
     $orgs = get_org_names();
@@ -69,75 +91,156 @@ function PostedFragment( &$r )
 }
 
 
+/* helper for formatting results */
+function DecodeJournoList( $jlist )
+{
+    $journos = array();
 
-function Doit( $kw )
+    if( $jlist )
+    {
+        foreach( explode( ',', $jlist ) as $s )
+        {
+            $parts = explode('|',$s);
+            $journos[] = array( 'ref'=>$parts[0], 'prettyname'=>$parts[1] );
+        }
+    }
+    return $journos;
+}
+
+
+function PageURL( $query, $start, $num_per_page )
+{
+    return sprintf( "/search?q=%s&start=%d&num=%d",
+        urlencode($query), $start, $num_per_page );
+}
+
+
+function EmitPageControl( $query, $start, $num_per_page, $total )
+{
+    $pagecnt = $total/$num_per_page;
+    $currentpage = $start/$num_per_page;
+
+    $firstpage = max( 0, $currentpage-3 );
+    $lastpage = min( $pagecnt, $currentpage+3 );
+
+    if( $start>0 && $total>0 )
+    {
+        printf( "<a href=\"%s\">%s</a> ",
+            PageURL($query,
+                max(0,$start-$num_per_page),
+                $num_per_page),
+            "Previous" );
+    }
+
+    for( $page=$firstpage; $page<=$lastpage; ++$page )
+    {
+        if( $page == $currentpage ) {
+            printf("%s ", $page+1 );
+        } else {
+            printf( "<a href=\"%s\">%s</a> ",
+                PageURL($query, $page*$num_per_page, $num_per_page),
+                $page+1 );
+        }
+    }
+
+    if( $start+$num_per_page < $total )
+    {
+        printf( "<a href=\"%s\">%s</a> ",
+            PageURL($query, $start+$num_per_page, $num_per_page),
+            "Next" );
+    }
+
+    print "<br/>\n";
+
+
+}
+
+
+
+/* perform query, display results */
+function DoQuery( $query_string, $start, $num_per_page )
 {
     global $DBPATH;
 
-try {
-    $orgs = get_org_names();
+    try {
+        $orgs = get_org_names();
 
-    $database = new XapianDatabase( $DBPATH );
+        $database = new XapianDatabase( $DBPATH );
 
-    // Start an enquire session.
-    $enquire = new XapianEnquire($database);
+        // Start an enquire session.
+        $enquire = new XapianEnquire($database);
 
-    $query_string = $kw;
+        $qp = new XapianQueryParser();
+        $stemmer = new XapianStem("english");
+        $qp->set_stemmer($stemmer);
+        $qp->set_database($database);
+        $qp->set_stemming_strategy(XapianQueryParser::STEM_SOME);
 
-    $qp = new XapianQueryParser();
-    $stemmer = new XapianStem("english");
-    $qp->set_stemmer($stemmer);
-    $qp->set_database($database);
-    $qp->set_stemming_strategy(XapianQueryParser::STEM_SOME);
+        $qp->add_prefix( 'byline', 'B' );
+        $qp->add_prefix( 'title', 'T' );
 
-    $qp->add_prefix( 'byline', 'B' );
-    $qp->add_prefix( 'title', 'T' );
+        $query = $qp->parse_query($query_string);
+    /*    print "<pre>Parsed query is: {$query->get_description()}</pre>\n"; */
 
-    $query = $qp->parse_query($query_string);
-    print "<pre>Parsed query is: {$query->get_description()}</pre>\n";
+        $enquire->set_query($query);
+        $matches = $enquire->get_mset($start, $num_per_page);
 
-    // Find the top 10 results for the query.
-    $enquire->set_query($query);
-    $matches = $enquire->get_mset(0,1000);
+        $total = max( $matches->get_matches_estimated(), $start+$matches->size() );
 
-    // Display the results.
-    print "<p>{$matches->get_matches_estimated()} results found:</p>\n";
-    print "<ul>\n";
-    $i = $matches->begin();
-    while (!$i->equals($matches->end())) {
-    	$n = $i->get_rank() + 1;
-        $doc = $i->get_document();
-    	$data = $doc->get_data();
 
-//ARTICLE_ID = 0
-//TITLE_ID = 1
-//PUBDATE_ID = 2
-//SRCORG_ID = 3
-//PERMALINK_ID = 4
-        $article_id = $doc->get_value( 0 );
+        // Display the results.
+        printf( "<p>Showing %d-%d of around %d:</p>\n",
+            $start+1,
+            min( $start+$num_per_page, $total),
+            $total );
 
-        /* a bunch of values are stored in the xapian db for us, so
-         * we don't have to look them up in the main db.
-         */
-        $art = array(
-            'id'=>$article_id,
-            'title'=> $doc->get_value(1),
-            'pubdate'=> $doc->get_value(2),
-            'srcorg' => $doc->get_value(3),
-            'permalink' => $doc->get_value(4) );
+        print "<ul>\n";
+        $i = $matches->begin();
+        while (!$i->equals($matches->end())) {
+            $n = $i->get_rank() + 1;
+            $doc = $i->get_document();
+            $data = $doc->get_data();
 
-        $headlinelink = "<a href=\"/article?id={$art['id']}\">{$art['title']}</a>\n";
-        $postedfrag = PostedFragment($art);
-        $url = "/article?id=" . $article_id;
-    	//print "<li>{$i->get_percent()}% \"{$headlinelink}\"<br/>{$postedfrag}</li>\n";
-    	print "<li>\"{$headlinelink}\"<br/>{$postedfrag}</li>\n";
-    	$i->next();
+            $article_id = $doc->get_value( XAP_ARTICLE_ID );
+
+            /* a bunch of values are stored in the xapian db for us, so
+             * we don't have to look them up in the main db.
+             */
+            $art = array(
+                'id'=>$article_id,
+                'title'=> $doc->get_value( XAP_TITLE_ID ),
+                'pubdate'=> $doc->get_value( XAP_PUBDATE_ID ),
+                'srcorg' => $doc->get_value( XAP_SRCORG_ID ),
+                'permalink' => $doc->get_value( XAP_PERMALINK_ID ),
+                'journos' => DecodeJournoList( $doc->get_value( XAP_JOURNOS_ID ) ) );
+
+            $headlinelink = "<a href=\"/article?id={$art['id']}\">{$art['title']}</a>\n";
+            $postedfrag = PostedFragment($art);
+
+            $journolinks = array();
+            foreach( $art['journos'] as $j )
+            {
+                $journolinks[] = sprintf( "<a href=\"%s\">%s</a>", '/'.$j['ref'], $j['prettyname'] );
+            }
+
+            $journofrag = '';
+            if( $journolinks )
+                $journofrag = ' <small>(' . implode( ', ', $journolinks ) . ")</small>";
+
+            //print "<li>{$i->get_percent()}% \"{$headlinelink}\"<br/>{$postedfrag}</li>\n";
+            print "<li>\"{$headlinelink}\"<br/>{$postedfrag}{$journofrag}</li>\n";
+            $i->next();
+        }
+        print "</ul>\n";
+
+        if( $total >= $num_per_page || $start>0 )
+        {
+            EmitPageControl( $query_string, $start, $num_per_page, $total );
+        }
+    } catch (Exception $e) {
+        print $e->getMessage() . "\n";
+
     }
-    print "</ul>\n";
-} catch (Exception $e) {
-    print $e->getMessage() . "\n";
-    exit(1);
-}
 }
 ?>
 
