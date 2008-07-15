b670d4e8b7a40a135e969c4d6215fce434129a81
Ben Campbell <ben@scumways.com>
2008-07-15 17:21:04 +0000

    times: minor change on their master papers page was causing scraper to bail
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@521 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/scraper/times.py
+++ jl/scraper/times.py
@@ -34,13 +34,13 @@ from JL import ukmedia, ScraperUtils
 # lots of links on the page which we don't want, so we'll
 # look for sections with links we _do_ want...
 sectionnames = ('News',
-		'incomingFeeds',
-		'Comment',
-		'Business',
-		'Sport',
-		'Life & Style',
-		'Arts & Entertainment',
-		)
+        'incomingFeeds',
+        'Comment',
+        'Business',
+        'Sport',
+        'Life & Style',
+        'Arts & Entertainment',
+        )
 
 siteroot = "http://timesonline.co.uk"
 
@@ -66,195 +66,199 @@ def CleanHtml(html):
 
 def FindArticles():
 
-	ukmedia.DBUG2( "*** times ***: looking for articles...\n" )
-	foundarticles = []
+    ukmedia.DBUG2( "*** times ***: looking for articles...\n" )
+    foundarticles = []
 
-	# hit the page which shows the covers of the papers for the week
-	# and extract a link to each day
-	ukmedia.DBUG2( "fetching /tol/newspapers/the_times...\n" )
-	html = ukmedia.FetchURL( siteroot + '/tol/newspapers/the_times' )
-#	ukmedia.DBUG2( "  got it.\n" )
-	soup = BeautifulSoup.BeautifulSoup(html)
+    # hit the page which shows the covers of the papers for the week
+    # and extract a link to each day
+    ukmedia.DBUG2( "fetching /tol/newspapers/the_times...\n" )
+    html = ukmedia.FetchURL( siteroot + '/tol/newspapers/the_times' )
+#   ukmedia.DBUG2( "  got it.\n" )
+    soup = BeautifulSoup.BeautifulSoup(html)
 
-	# (one day of the week will always be missing, as it'll have
-	# been renamed 'Today')
-	days = ( 'Today', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'The Sunday Times' )
+    # (one day of the week will always be missing, as it'll have
+    # been renamed 'Today')
+    days = ( 'Today', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'The Sunday Times' )
+    daypat = re.compile( "/tol/newspapers/(.*?)/[?]days=(.*?)" )
 
-	daypages = {}
-	for link in soup.findAll( 'a', {'class':"link-06c"} ):
-		day = link.renderContents(None).strip()
-		if day in days:
-			daypages[day] = siteroot + link['href']
+    daypages = {}
+    for link in soup.findAll( 'a', {'class':"link-06c"} ):
 
-	# go through each days page and extract links to articles
-	for day, url in daypages.iteritems():
+        url = link['href']
+        if daypat.match( url ):
+            day = link.renderContents(None).strip()
+            if day in days:
+                daypages[day] = siteroot + url
 
-		ukmedia.DBUG2( "fetching " + day + "\n" )
-		html = ukmedia.FetchURL( url )
-#		ukmedia.DBUG( " got " + day + "\n" )
-		fetchtime = datetime.now()
-		soup = BeautifulSoup.BeautifulSoup(html)
+    # go through each days page and extract links to articles
+    for day, url in daypages.iteritems():
+
+        ukmedia.DBUG2( "fetching " + day + "\n" )
+        html = ukmedia.FetchURL( url )
+#       ukmedia.DBUG( " got " + day + "\n" )
+        fetchtime = datetime.now()
+        soup = BeautifulSoup.BeautifulSoup(html)
 
 
-		# Which newspaper?
-		if re.search( "/?days=Sunday$", url ):
-			srcorgname = "sundaytimes"
-		else:
-			srcorgname = "times"
-		ukmedia.DBUG2( "** PAPER: " + srcorgname + "\n" )
+        # Which newspaper?
+        if re.search( "/?days=Sunday$", url ):
+            srcorgname = "sundaytimes"
+        else:
+            srcorgname = "times"
+        ukmedia.DBUG2( "** PAPER: " + srcorgname + "\n" )
 
-		# go through by section
-		for heading in soup.findAll( 'h3', {'class': 'section-heading' } ):
-			sectionname = heading.find( text = sectionnames )
-			if not sectionname:
-#				print "Ignoring section ",heading
-				continue
+        # go through by section
+        for heading in soup.findAll( 'h3', {'class': 'section-heading' } ):
+            sectionname = heading.find( text = sectionnames )
+            if not sectionname:
+#               print "Ignoring section ",heading
+                continue
 
-			ukmedia.DBUG2( "  " + sectionname + "\n" )
+            ukmedia.DBUG2( "  " + sectionname + "\n" )
 
-			ul = heading.findNextSibling( 'ul' )
-			for a in ul.findAll( 'a' ):
-				title = ukmedia.DescapeHTML( a.renderContents(None) )
-				url = siteroot + a['href']
+            ul = heading.findNextSibling( 'ul' )
+            for a in ul.findAll( 'a' ):
+                title = ukmedia.DescapeHTML( a.renderContents(None) )
+                url = siteroot + a['href']
 
-				context = {
-					'title': title,
-					'srcurl': url,
-					'srcid': CalcSrcID(url),
-					'permalink': url,
-					'lastseen': fetchtime,
-					'srcorgname' : srcorgname,
-					}
+                context = {
+                    'title': title,
+                    'srcurl': url,
+                    'srcid': CalcSrcID(url),
+                    'permalink': url,
+                    'lastseen': fetchtime,
+                    'srcorgname' : srcorgname,
+                    }
 
-				foundarticles.append( context )
+                foundarticles.append( context )
 
-	ukmedia.DBUG2( "Found %d articles\n" % ( len(foundarticles) ) )
-	return foundarticles
+    ukmedia.DBUG2( "Found %d articles\n" % ( len(foundarticles) ) )
+    return foundarticles
 
 
 # http://www.timesonline.co.uk/tol/news/politics/article3471714.ece
 srcidpat_ece = re.compile( '/(article[0-9]+\.ece)$' )
 
 def CalcSrcID( url ):
-	""" work out a unique srcid for this url """
+    """ work out a unique srcid for this url """
 
-	if not url.startswith('http') and ('-d' in sys.argv or '--dryrun' in sys.argv):
-		return 'DRYRUN:times.py:' + url
-	
-	o = urlparse.urlparse( url )
+    if not url.startswith('http') and ('-d' in sys.argv or '--dryrun' in sys.argv):
+        return 'DRYRUN:times.py:' + url
+    
+    o = urlparse.urlparse( url )
 
-	if o[1].endswith( 'typepad.com' ):
-		# times blogs handled in blogs.py
-		return None
+    if o[1].endswith( 'typepad.com' ):
+        # times blogs handled in blogs.py
+        return None
 
-	if not o[1].endswith( 'timesonline.co.uk' ):
-		return None
+    if not o[1].endswith( 'timesonline.co.uk' ):
+        return None
 
-	m = srcidpat_ece.search( o[2] )
-	if m:
-		return 'times_' + m.group(1)
+    m = srcidpat_ece.search( o[2] )
+    if m:
+        return 'times_' + m.group(1)
 
-	return None
+    return None
 
 
 def Extract( html, context ):
 
-	art = context
-	html = CleanHtml(html)
-	soup = BeautifulSoup.BeautifulSoup( html )
+    art = context
+    html = CleanHtml(html)
+    soup = BeautifulSoup.BeautifulSoup( html )
 
-	h1 = soup.find( 'h1', {'class':'heading'} )
-	art['title'] = h1.renderContents(None).strip()
-	art['title'] = ukmedia.DescapeHTML( ukmedia.StripHTML( art['title'] ) )
+    h1 = soup.find( 'h1', {'class':'heading'} )
+    art['title'] = h1.renderContents(None).strip()
+    art['title'] = ukmedia.DescapeHTML( ukmedia.StripHTML( art['title'] ) )
 
-	byline = u''
-	# times stuffs up bylines for obituaries (used for date span instead)
-	if art['srcurl'].find( '/obituaries/' ) == -1:
-		authdiv = soup.find( 'div', {'class':'article-author'} )
-		if authdiv:
-			bylinespan = authdiv.find( 'span', { 'class': 'byline' } )
-			if bylinespan:
-				byline = bylinespan.renderContents( None )
-				byline = ukmedia.StripHTML( byline )
-				byline = ukmedia.DescapeHTML( byline )
-				byline = u' '.join( byline.split() )
-	art['byline'] = byline
-
-
-	# Extract the article text and build it into it's own soup
-	rawcontent_pat = re.compile( u"<!-- Pagination -->(.*)<!-- End of pagination -->", re.UNICODE|re.DOTALL )
-
-	m = rawcontent_pat.search( html )
-	contentsoup = BeautifulSoup.BeautifulSoup( m.group(1), fromEncoding = soup.originalEncoding )
+    byline = u''
+    # times stuffs up bylines for obituaries (used for date span instead)
+    if art['srcurl'].find( '/obituaries/' ) == -1:
+        authdiv = soup.find( 'div', {'class':'article-author'} )
+        if authdiv:
+            bylinespan = authdiv.find( 'span', { 'class': 'byline' } )
+            if bylinespan:
+                byline = bylinespan.renderContents( None )
+                byline = ukmedia.StripHTML( byline )
+                byline = ukmedia.DescapeHTML( byline )
+                byline = u' '.join( byline.split() )
+    art['byline'] = byline
 
 
-#	paginationstart = soup.find( text=re.compile('^\s*Pagination\s*$') )
-#	paginationend = soup.find( text=re.compile('^\s*End of pagination\s*$') )
+    # Extract the article text and build it into it's own soup
+    rawcontent_pat = re.compile( u"<!-- Pagination -->(.*)<!-- End of pagination -->", re.UNICODE|re.DOTALL )
 
-#	if not paginationstart:
-#		raise Exception, "couldn't find start of main text!"
-#	if not paginationend:
-#		raise Exception, "couldn't find end of main text!"
+    m = rawcontent_pat.search( html )
+    contentsoup = BeautifulSoup.BeautifulSoup( m.group(1), fromEncoding = soup.originalEncoding )
 
 
+#   paginationstart = soup.find( text=re.compile('^\s*Pagination\s*$') )
+#   paginationend = soup.find( text=re.compile('^\s*End of pagination\s*$') )
 
-#	contentsoup = BeautifulSoup.BeautifulSoup()
-#	p = paginationstart.nextSibling
-#	while p != paginationend:
-#		print p.name
-#		next = p.nextSibling
-#		if not isinstance( p, BeautifulSoup.Comment ):
-#			contentsoup.insert( len(contentsoup.contents), p )
-#		p = next
+#   if not paginationstart:
+#       raise Exception, "couldn't find start of main text!"
+#   if not paginationend:
+#       raise Exception, "couldn't find end of main text!"
 
 
-	for cruft in contentsoup.findAll( 'div', {'class':'float-left related-attachements-container' } ):
-		cruft.extract()
-	for cruft in contentsoup.findAll( 'script' ):
-		cruft.extract()
-	#...more?
 
-	art['content'] = ukmedia.SanitiseHTML( contentsoup.prettify(None) )
+#   contentsoup = BeautifulSoup.BeautifulSoup()
+#   p = paginationstart.nextSibling
+#   while p != paginationend:
+#       print p.name
+#       next = p.nextSibling
+#       if not isinstance( p, BeautifulSoup.Comment ):
+#           contentsoup.insert( len(contentsoup.contents), p )
+#       p = next
 
-	# skip crossword solutions etc...
-	if art['content'].strip() == u'' and art['srcurl'].find( "games_and_puzzles" ) != -1:
-		ukmedia.DBUG2( "IGNORE puzzle solution: '%s' (%s)\n" % (art['title'], art['srcurl']) );
-		return None
 
-	# description is in a meta tag
-	descmeta = soup.find('meta', {'name':'Description'} )
-	desc = descmeta['content']
-	desc = ukmedia.DescapeHTML( desc )
-	desc = ukmedia.RemoveTags( desc )
-	art['description' ] = desc
+    for cruft in contentsoup.findAll( 'div', {'class':'float-left related-attachements-container' } ):
+        cruft.extract()
+    for cruft in contentsoup.findAll( 'script' ):
+        cruft.extract()
+    #...more?
+
+    art['content'] = ukmedia.SanitiseHTML( contentsoup.prettify(None) )
+
+    # skip crossword solutions etc...
+    if art['content'].strip() == u'' and art['srcurl'].find( "games_and_puzzles" ) != -1:
+        ukmedia.DBUG2( "IGNORE puzzle solution: '%s' (%s)\n" % (art['title'], art['srcurl']) );
+        return None
+
+    # description is in a meta tag
+    descmeta = soup.find('meta', {'name':'Description'} )
+    desc = descmeta['content']
+    desc = ukmedia.DescapeHTML( desc )
+    desc = ukmedia.RemoveTags( desc )
+    art['description' ] = desc
 
 
 #NEW VERSION
-	# the pubdate is in a comment, eg:
-	# <!-- Article Published Date : Apr 23, 2008 12:00 AM -->
-	pubdate_pat = re.compile( "<!-- Article Published Date\\s*:\\s*(.*)\\s*-->" )
-	m = pubdate_pat.search( html )
-	art['pubdate'] = ukmedia.ParseDateTime( m.group(1) )
+    # the pubdate is in a comment, eg:
+    # <!-- Article Published Date : Apr 23, 2008 12:00 AM -->
+    pubdate_pat = re.compile( "<!-- Article Published Date\\s*:\\s*(.*)\\s*-->" )
+    m = pubdate_pat.search( html )
+    art['pubdate'] = ukmedia.ParseDateTime( m.group(1) )
 
 #OLD VERSION
-	# There is some javascript with a likely-looking pubdate:
-	# var tempDate="02-Jan-2006 00:00";
-#	datepat = re.compile( u"\s*var tempDate=\"(.*?)\";", re.UNICODE )
-#	m = datepat.search(html)
-#	art['pubdate'] = ukmedia.ParseDateTime( m.group(1) )
+    # There is some javascript with a likely-looking pubdate:
+    # var tempDate="02-Jan-2006 00:00";
+#   datepat = re.compile( u"\s*var tempDate=\"(.*?)\";", re.UNICODE )
+#   m = datepat.search(html)
+#   art['pubdate'] = ukmedia.ParseDateTime( m.group(1) )
 
-	return art
+    return art
 
 
 def ContextFromURL( url ):
-	"""Build up an article scrape context from a bare url."""
-	context = {}
-	context['srcurl'] = url
-	context['permalink'] = url
-	context['srcid'] = CalcSrcID( url )
-	context['srcorgname'] = u'times'	# TODO: or sundaytimes!
-	context['lastseen'] = datetime.now()
-	return context
+    """Build up an article scrape context from a bare url."""
+    context = {}
+    context['srcurl'] = url
+    context['permalink'] = url
+    context['srcid'] = CalcSrcID( url )
+    context['srcorgname'] = u'times'    # TODO: or sundaytimes!
+    context['lastseen'] = datetime.now()
+    return context
 
 
 
