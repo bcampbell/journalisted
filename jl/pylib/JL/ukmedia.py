6683121688aff2e847091aa39b2f736eff7726a9
Ben Campbell <ben@scumways.com>
2008-10-16 12:59:20 +0000

    scraper dry-run prettydump fn now prints out credit field of images
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@595 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/pylib/JL/ukmedia.py
+++ jl/pylib/JL/ukmedia.py
@@ -536,7 +536,7 @@ def PrettyDump( art ):
             print "%11s: %s" % (f,unicode( art[f] ).encode('utf-8') )
     if 'images' in art:
         for i in art['images']:
-            print "image: %s ('%s')" % ( i['url'], i['caption'].encode('utf-8') )
+            print "image: %s ('%s') [%s]" % ( i['url'], i['caption'].encode('utf-8'), i['credit'].encode('utf-8') )
     if 'commentlinks' in art:
         for c in art['commentlinks']:
             print "comments: %d at %s" % ( c['num_comments'], c['comment_url'])
