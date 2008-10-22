48c4cc7b6cff94768385ea94b752de557f944ba2
Ben Campbell <ben@scumways.com>
2008-10-22 20:12:04 +0000

    misc fixes to journo rss feeds - should now be proper and valid (fingers crossed!)
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@602 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/web/journo_rss.php
+++ jl/web/journo_rss.php
@@ -17,7 +17,8 @@ $ref = strtolower( get_http_var( 'ref' ) );
 
 $journo = db_getRow( "SELECT id,ref,prettyname,lastname,firstname FROM journo WHERE ref=? AND status='a'", $ref );
 
-header('Content-type: text/xml;');
+#header('Content-type: text/xml;');
+header('Content-type: application/rss+xml;');
 
 emit_recent_articles( $journo );
 
@@ -25,17 +26,19 @@ emit_recent_articles( $journo );
 
 function emit_recent_articles( $journo )
 {
-	print "<?xml version=\"1.0\"?>\n";
-	print "<rss version=\"2.0\">\n";
+	print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	print "<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n";
 	print "\t<channel>\n";
 	printf( "\t\t<title>Recent articles by %s</title>\n", $journo['prettyname'] );
 	printf( "\t\t<link>http://%s/%s</link>\n", OPTION_WEB_DOMAIN, $journo['ref'] );
-
+    /* include a self link to this feed */
+    printf( "\t\t<atom:link href=\"%s\" rel=\"self\" type=\"application/rss+xml\" />\n",
+        OPTION_BASE_URL . '/' . $journo['ref'] . '/rss' );
 	printf( "\t\t<description></description>\n" );
 	print( "\t\t<language>en-gb</language>\n" );
 
 	$journo_id = $journo[ 'id' ];
-	$sql = "SELECT a.id,a.title,a.description,a.pubdate,a.permalink,a.srcorg " .
+	$sql = "SELECT a.id,a.title,a.description,date_part('epoch',a.pubdate) as unixtime,a.permalink,a.srcorg " .
 		"FROM article a,journo_attr j " .
 		"WHERE a.status='a' AND a.id=j.article_id AND " .
 			"j.journo_id=? AND a.pubdate>(NOW()-interval '10 months') " .
@@ -46,10 +49,11 @@ function emit_recent_articles( $journo )
 	while( $art = db_fetch_array($q) )
 	{
 		print( "\t\t<item>\n" );
-		printf( "\t\t\t<title>%s</title>\n", $art['title'] );
+		printf( "\t\t\t<title>%s</title>\n", htmlspecialchars( $art['title'],ENT_COMPAT,'UTF-8') );
 		printf( "\t\t\t<link>%s</link>\n", $art['permalink'] );
-		printf( "\t\t\t<description>%s</description>\n", $art['description'] );
-		printf( "\t\t\t<pubDate>%s</pubDate>\n", $art['pubdate'] );
+		printf( "\t\t\t<description>%s</description>\n", htmlspecialchars( $art['description'],ENT_COMPAT,'UTF-8') );
+        #Wed, 02 Oct 2002 15:00:00 +0200
+		printf( "\t\t\t<pubDate>%s</pubDate>\n", date( "D, d M Y H:i:s O", $art['unixtime']) );
 		printf( "\t\t\t<guid>%s</guid>\n", $art['permalink'] );
 		print( "\t\t</item>\n" );
 	}
