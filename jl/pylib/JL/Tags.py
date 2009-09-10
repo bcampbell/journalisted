d862b1b99476a03e5a52097e726b0078685dbf78
Ben Campbell <ben@scumways.com>
2009-09-10 15:34:42 +0000

    retabbed
    git-svn-id: https://journa-list.googlecode.com/svn/trunk@813 dbcfc70c-e32f-0410-8862-5d2c5c102a8f

--- jl/pylib/JL/Tags.py
+++ jl/pylib/JL/Tags.py
@@ -13,136 +13,136 @@ countrylist_cached = None
 blacklist_cached = None
 
 
-def GetOverallTagFrequency( conn, tag ):	# gtb
-	c = conn.cursor()
-	c.execute("""
-		SELECT SUM(t.freq) AS freq 
-		FROM article_tag t 
-		WHERE t.tag='"""+tag+"'"+""" 
-	""");
-	row = c.fetchone()
-	c.close()
-	if not row:
-		return 0	# tag not found
-	return row[0]
+def GetOverallTagFrequency( conn, tag ):    # gtb
+    c = conn.cursor()
+    c.execute("""
+        SELECT SUM(t.freq) AS freq 
+        FROM article_tag t 
+        WHERE t.tag='"""+tag+"'"+""" 
+    """);
+    row = c.fetchone()
+    c.close()
+    if not row:
+        return 0    # tag not found
+    return row[0]
 
 
 
 def GetCountryList():
-	"""load list of nations and nationalities"""
+    """load list of nations and nationalities"""
 
-	global countrylist_cached
+    global countrylist_cached
 
-	if countrylist_cached != None:
-		return countrylist_cached
+    if countrylist_cached != None:
+        return countrylist_cached
 
-	countrylist_cached = []
+    countrylist_cached = []
 
 
-	countrydatafile = os.path.join(os.path.dirname(__file__),'countries.csv')
-	f = open( countrydatafile, "rb" )
+    countrydatafile = os.path.join(os.path.dirname(__file__),'countries.csv')
+    f = open( countrydatafile, "rb" )
 
-	reader = csv.reader( f )
-	for row in reader:
-		c = row[0].decode( 'utf-8' ).lower()
-		countrylist_cached.append( c )
-	return countrylist_cached
+    reader = csv.reader( f )
+    for row in reader:
+        c = row[0].decode( 'utf-8' ).lower()
+        countrylist_cached.append( c )
+    return countrylist_cached
 
 
 def GetBlacklist():
-	"""get the list of banned tags from the db"""
+    """get the list of banned tags from the db"""
 
-	global blacklist_cached
+    global blacklist_cached
 
-	# don't hit db more often that necessary
-	if blacklist_cached != None:
-		return blacklist_cached
+    # don't hit db more often that necessary
+    if blacklist_cached != None:
+        return blacklist_cached
 
-	conn = DB.Connect()
-	c = conn.cursor()
-	c.execute( "SELECT bannedtag FROM tag_blacklist" )
+    conn = DB.Connect()
+    c = conn.cursor()
+    c.execute( "SELECT bannedtag FROM tag_blacklist" )
 
-	blacklist_cached = []
-	while 1:
-		r = c.fetchone()
-		if not r:
-			break
-		tag = r[0].decode('utf-8')
-		blacklist_cached.append( tag )
+    blacklist_cached = []
+    while 1:
+        r = c.fetchone()
+        if not r:
+            break
+        tag = r[0].decode('utf-8')
+        blacklist_cached.append( tag )
 
-	c.close()
-	return blacklist_cached
+    c.close()
+    return blacklist_cached
 
 
 def ExtractFromText( txt ):
-	"""extract a list of terms (tags) from text"""
+    """extract a list of terms (tags) from text"""
 
-	blacklist = GetBlacklist()
-	countries = GetCountryList()
-	synonyms = {}
+    blacklist = GetBlacklist()
+    countries = GetCountryList()
+    synonyms = {}
 
-	# extract phrases with the first letter of each word capitalised,
-	# but not at the beginning of a sentence.
-	tagpat = re.compile( u'[^.\\s]\\s*(([A-Z]\\w+)(\\s+([A-Z]\\w+))*)', re.UNICODE|re.DOTALL )
+    # extract phrases with the first letter of each word capitalised,
+    # but not at the beginning of a sentence.
+    tagpat = re.compile( u'[^.\\s]\\s*(([A-Z]\\w+)(\\s+([A-Z]\\w+))*)', re.UNICODE|re.DOTALL )
 
-	# prefixes we'll trim off
-	prefixpat = re.compile( u'^(mr|dr|ms|mrs)\\s+',re.UNICODE|re.IGNORECASE )
+    # prefixes we'll trim off
+    prefixpat = re.compile( u'^(mr|dr|ms|mrs)\\s+',re.UNICODE|re.IGNORECASE )
 
-	# calculate tags using noddy Crapitisation algorithm
-	tags = {}
-	for m in tagpat.findall(txt):
-		# compress whitespace
-		words = m[0].split()
-		if len( words ) > 4:
-			continue	# discard tags with more than 4 words
-		t = ' '.join( words )
+    # calculate tags using noddy Crapitisation algorithm
+    tags = {}
+    for m in tagpat.findall(txt):
+        # compress whitespace
+        words = m[0].split()
+        if len( words ) > 4:
+            continue    # discard tags with more than 4 words
+        t = ' '.join( words )
 
-		# ignore short tags unless they look like acronymns
-		if len(t)<=3 and t != t.upper():
-			continue
+        # ignore short tags unless they look like acronymns
+        if len(t)<=3 and t != t.upper():
+            continue
 
-		t = t.lower()
-		if t in blacklist:
-			continue
+        t = t.lower()
+        if t in blacklist:
+            continue
 
-		# trim off any title prefixes (mr, mrs, ms, dr)
-		t = prefixpat.sub( u'', t )
+        # trim off any title prefixes (mr, mrs, ms, dr)
+        t = prefixpat.sub( u'', t )
 
-		# is there a synonym to remap this key?
-		if synonyms.has_key(t):
-			t=synonyms[t]
-
-
-		# which kind of term is it?
-		kind = ' '	# unknown
-		if t in countries:
-			kind = 'c'
+        # is there a synonym to remap this key?
+        if synonyms.has_key(t):
+            t=synonyms[t]
 
 
-		# TODO: kind field in DB should be part of primary key too!
+        # which kind of term is it?
+        kind = ' '  # unknown
+        if t in countries:
+            kind = 'c'
 
-		# key is tag name _and_ kind of tag!
-		k = (t,kind)
-		tags[k] = tags.get(k,0) + 1		# ++freq
 
-	return tags
+        # TODO: kind field in DB should be part of primary key too!
+
+        # key is tag name _and_ kind of tag!
+        k = (t,kind)
+        tags[k] = tags.get(k,0) + 1     # ++freq
+
+    return tags
 
 
 def Generate( conn, article_id, article_content ):
-	""" Generate tags for an article """
+    """ Generate tags for an article """
 
 
-	txt = ukmedia.StripHTML( article_content )
+    txt = ukmedia.StripHTML( article_content )
 
-	tags = ExtractFromText( txt )
+    tags = ExtractFromText( txt )
 
-	# write the tags into the DB
-	c2 = conn.cursor()
-	for tagkey,tagfreq in tags.items():
+    # write the tags into the DB
+    c2 = conn.cursor()
+    for tagkey,tagfreq in tags.items():
 
-		tagname = tagkey[0].encode('utf-8')
-		tagkind = tagkey[1]
-		c2.execute( "INSERT INTO article_tag (article_id, tag, kind, freq) VALUES (%s,%s,%s,%s)",
-			article_id, tagname, tagkind, tagfreq )
-	c2.close()
+        tagname = tagkey[0].encode('utf-8')
+        tagkind = tagkey[1]
+        c2.execute( "INSERT INTO article_tag (article_id, tag, kind, freq) VALUES (%s,%s,%s,%s)",
+            article_id, tagname, tagkind, tagfreq )
+    c2.close()
 
