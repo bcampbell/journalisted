6e001d376f50c5ec96a8171f051d4d644e1436c5
bcampbell <ben@scumways.com>
2009-10-07 13:55:56 +0100

    automagically jump to appropriate journo profile for the logged in user, if no journo specified

--- jl/web/profile.php
+++ jl/web/profile.php
@@ -13,17 +13,37 @@ require_once '../../phplib/utility.php';
 
 /* get journo identifier (eg 'fred-bloggs') */
 
+$P = person_if_signed_on();
 $ref = strtolower( get_http_var( 'ref' ) );
-$journo = db_getRow( "SELECT * FROM journo WHERE status='a' AND ref=?", $ref );
+
+$journo = NULL;
+if( $ref ) {
+    $journo = db_getRow( "SELECT * FROM journo WHERE status='a' AND ref=?", $ref );
+} else {
+    // no journo given - if person is logged on, see if they are associated with a journo
+    if( $P ) {
+        $journo_id = db_getOne( "SELECT journo_id FROM person_permission WHERE person_id=? AND permission='edit'", $P->id() );
+        if( $journo_id ) {
+            $journo = db_getRow( "SELECT * FROM journo WHERE status='a' AND id=?", $journo_id );
+        }
+    }
+}
+
 if(!$journo)
 {
+    if($P ) {
+        page_header("");
+?>
+Sorry, your account hasn't yet been linked to a journalist page.
+<?php
+        page_footer();
+        exit;
+    }
     header("HTTP/1.0 404 Not Found");
     exit(1);
 }
 
-
 // if logged in and have access, redirect.
-$P = person_if_signed_on();
 if( $P ) {
     // is this person allowed to edit this journo?
     if( db_getOne( "SELECT id FROM person_permission WHERE person_id=? AND journo_id=? AND permission='edit'",
