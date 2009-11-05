304bccceffcee27c6ba8a3b4f890d0e3fbc8a693
bcampbell <ben@scumways.com>
2009-11-05 11:21:25 +0000

    clarified profile page login

--- jl/web/profile.php
+++ jl/web/profile.php
@@ -14,19 +14,25 @@ require_once '../../phplib/utility.php';
 /* get journo identifier (eg 'fred-bloggs') */
 
 $ref = strtolower( get_http_var( 'ref' ) );
-$r = array(
+$journo = NULL;
+
+if( $ref ) {
+    $journo = db_getRow( "SELECT * FROM journo WHERE status='a' AND ref=?", $ref );
+    if( !$journo ) {
+        header("HTTP/1.0 404 Not Found");
+        exit(1);
+    }
+}
+
+$login_reasons = array(
     'reason_web' => "Edit your page on Journalisted",
     'reason_email' => "Edit your page on Journalisted",
     'reason_email_subject' => "Edit your page on Journalisted"
     );
-    // rediect to login screen if not logged in (so this may never return)
-$P = person_signon($r);
 
+$P = person_if_signed_on();
 
-$journo = NULL;
-if( $ref ) {
-    $journo = db_getRow( "SELECT * FROM journo WHERE status='a' AND ref=?", $ref );
-} else {
+if( is_null($journo) ) {
     // no journo given - if person is logged on, see if they are associated with a journo (or journos)
     if( $P ) {
         $editables = db_getAll( "SELECT j.* FROM ( journo j INNER JOIN person_permission p ON p.journo_id=j.id) WHERE p.person_id=? AND p.permission='edit' AND j.status='a'", $P->id() );
@@ -53,22 +59,10 @@ if( $ref ) {
     }
 }
 
-if(!$journo)
-{
-    if($P ) {
-        page_header("");
-?>
-Sorry, your account hasn't yet been linked to a journalist page.
-<?php
-        page_footer();
-        exit;
-    }
-    header("HTTP/1.0 404 Not Found");
-    exit(1);
-}
 
-// if logged in and have access, redirect.
-if( $P ) {
+
+// if logged in and have access to the journo, redirect.
+if( $journo && $P ) {
     // is this person allowed to edit this journo?
     if( db_getOne( "SELECT id FROM person_permission WHERE person_id=? AND journo_id=? AND permission='edit'",
         $P->id(), $journo['id'] ) ) {
@@ -80,11 +74,21 @@ if( $P ) {
 
 
 
-$title = "Edit profile for " . $journo['prettyname'];
+$title = "Edit profile";
+if( $journo )
+    $title = "Edit profile for " . $journo['prettyname'];
 page_header( $title );
 
 ?>
+<?php if( $journo ) { ?>
 <h2>Edit profile for <a href="/<?=$journo['ref'];?>"><?=$journo['prettyname'];?></a></h2>
+<?php } else { ?>
+<h2>Edit profile</h2>
+<?php } ?>
+
+<p>already got an account? <a href="/login">log in now!</a>
+
+<p><strong>or...</strong></p>
 
 <p>It's easy to get set up to edit your profile! But we need to know you are who you say you are...</p>
 Here's how:
@@ -96,7 +100,6 @@ Here's how:
 <li>blah blah we'll send you a login link blah blah</li>
 </ol>
 
-<p>Jumped through all the hoops and have an account already? <a href="/profile_admired?ref=<?=$journo['ref'];?>">log in now!</a>
 <?php
 
 page_footer();
