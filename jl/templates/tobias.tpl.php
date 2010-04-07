1b5eec7000b406ade32ecae826edec6b230b05d5
Ben Campbell <ben@midori.(none)>
2010-04-07 11:45:21 +0100

    insert standin-flash for tobias grubbe

--- jl/templates/tobias.tpl.php
+++ jl/templates/tobias.tpl.php
@@ -84,6 +84,11 @@ His photo a secret nature. However it is a truth that it is easier to find
 <div class="">
   <div class="head"><h3>Tobias Grubbe's latest adventures</h3></div>
   <div class="body">
+
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="600" height="484">
+<param name="movie" value="http://tobiasgrubbe.com/TG_opener.swf">
+<param name="quality" value="high"><embed src="http://tobiasgrubbe.com/TG_opener.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="600" height="484"></embed></object>
+
   </div>
   <div class="foot"></div>
 </div>
