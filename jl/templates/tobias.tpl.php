176a1d54cff4d97ee0499222d20d34c8f69f5fb1
Ben Campbell <ben@midori.(none)>
2010-04-08 17:54:44 +0100

    add TG trailer swf

--- jl/templates/tobias.tpl.php
+++ jl/templates/tobias.tpl.php
@@ -85,8 +85,8 @@ $previous_employers = array_unique( $previous_employers );
   <div class="body">
 
 <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="600" height="484">
-<param name="movie" value="http://tobiasgrubbe.com/TG_opener.swf">
-<param name="quality" value="high"><embed src="http://tobiasgrubbe.com/TG_opener.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="600" height="484"></embed></object>
+<param name="movie" value="/tobias/TG_Journalisted_trail.swf">
+<param name="quality" value="high"><embed src="/tobias/TG_Journalisted_trail.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="600" height="484"></embed></object>
 
   </div>
   <div class="foot"></div>
