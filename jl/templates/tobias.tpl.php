9454c53d2c890a1bc2ae1ee8c0cb47dd8b3c8573
Ben Campbell <ben@scumways.com>
2010-05-04 09:47:30 +0100

    new TG

--- jl/templates/tobias.tpl.php
+++ jl/templates/tobias.tpl.php
@@ -22,7 +22,16 @@ EOT
 <param name="movie" value="TG_GU_No3_260410.swf">
 <param name="quality" value="high"><embed src="http://www.tobiasgrubbe.com/TG_GU_No3_260410.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="600" height="484"></embed></object>
 EOT
+,
+<<<EOT
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="600" height="400">
+<param name="movie" value="TG_GU_No4_03_05_10.swf">
+<param name="quality" value="high">
+<embed src="/tobias/TG_GU_No4_03_05_10.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="600" height="484"></embed>
+</object>
+EOT
 );
+
 
 $episode = get_http_var( 'episode', sizeof($tg_objs) );
 if( $episode<1 || $episode > sizeof($tg_objs) )
