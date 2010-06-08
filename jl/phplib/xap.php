ab73a0012bab1f687c57c2c606894abf088c0edc
Ben Campbell <ben@scumways.com>
2010-06-08 15:28:32 +0100

    up forced limit on search result retrieval

--- jl/phplib/xap.php
+++ jl/phplib/xap.php
@@ -94,7 +94,7 @@ class XapSearch {
         }   /* (default is relevance) */
 
 
-        $checkatleast = max( $limit, 500 );
+        $checkatleast = max( $limit, 1000 );
         $matches = $this->enquire->get_mset($offset, $limit, $checkatleast );
 
         $this->total_results = max( $matches->get_matches_estimated(), $offset+$matches->size() );
