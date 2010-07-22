e40b607d60cf64a4d4447179413468ec81faf543
Ben Campbell <ben@scumways.com>
2010-07-22 11:02:07 +0100

    fix up wording for fake journos explanation page

--- jl/web/fakes.php
+++ jl/web/fakes.php
@@ -13,10 +13,13 @@ page_header( "Fake journalists" );
 <div class="body">
 <h2>A fake journalist?</h2>
 <ul>
-<li>newspapers have been doing this for ever</li>
-<li>slightly odd practice if you're not in the business</li>
-<li>we have emailed them, asked colleagues (via Twitter) and called the switchboard and not been able to confirm their identity</li>
-<li>but if it is you, then please <a href="/profile">claim</a> your profile page and we'll remove the strap straight away</li>
+<li>Sometimes news outlets invent fake journalists</li>
+<li>If you're not in the business this practice may strike you as rather odd (it does us)</li>
+<li>We think this journalist may not exist</li>
+<li>We have emailed him/her, asked colleagues (via Twitter) and called the switchboard and not been able to confirm his/her identity</li>
+<li>If you are this journalist then please <a href="/profile">claim</a> your profile page and we'll remove the strap straight away</li>
+<li>If you know that this journalist exists, please contact us and tell us how we can verify his/her existence</li>
+<li>In the meantime, we'll leave the public health warning up there</li>
 </ul>
 
 </div>
