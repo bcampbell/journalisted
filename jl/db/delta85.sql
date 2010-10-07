5fbbbf2fced5fb8f469335dca8b165ba64907433
bcampbell <ben@scumways.com>
2010-10-07 14:58:17 +0100

    add publication sets

---
+++ jl/db/delta85.sql
@@ -0,0 +1,17 @@
+BEGIN;
+
+BEGIN;
+
+CREATE TABLE pub_set (
+    id serial PRIMARY KEY,
+    name text NOT NULL
+);
+
+
+CREATE TABLE pub_set_map (
+    id serial PRIMARY KEY,
+    pub_id integer REFERENCES organisation(id) ON DELETE CASCADE,
+    pub_set_id integer REFERENCES pub_set(id) ON DELETE CASCADE
+);
+
+COMMIT;
