dccfd9885735c2f6797c2f1aa62325a06bdfdae3
Ben Campbell <ben@scumways.com>
2010-12-22 15:10:45 +0000

    increase error tolerance for sun scraper

--- jl/scraper/sun.py
+++ jl/scraper/sun.py
@@ -357,5 +357,5 @@ def ContextFromURL( url ):
 
 
 if __name__ == "__main__":
-    ScraperUtils.RunMain( FindArticles, ContextFromURL, Extract )
+    ScraperUtils.RunMain( FindArticles, ContextFromURL, Extract, maxerrors=50 )
 
