cea1191f2e85baa5638370622fd4c6d609a16373
Ben Campbell <ben@scumways.com>
2010-12-22 20:21:25 +0000

    add "before" param to dumpArticles api

--- jl/web/dumpArticles.php
+++ jl/web/dumpArticles.php
@@ -30,10 +30,8 @@ try {
     }
 
     $after = get_http_var('after' );
+    $before = get_http_var('before' );
 
-    if( !$after ) {
-        throw new Exception( "required parameter: after" );
-    }
 
     $MAX_LIMIT=1000;
     $limit = intval( get_http_var('limit',1));
@@ -42,21 +40,42 @@ try {
         throw new Exception( "limit out of range (max $MAX_LIMIT)" );
     }
 
-    $after_dt = date_create( $after );
-    if( !$after_dt ) {
-        throw new Exception( "bad date: after" );
+    $r=null;
+    if( !$after && !$before ) {
+        throw new Exception( "required parameter: after or before" );
+    } elseif( $after && $before ) {
+        throw new Exception( "bad parameter: after and before are mutually exclusive" );
+    } elseif( $after ) {
+        $after_dt = date_create( $after );
+        if( !$after_dt ) {
+            throw new Exception( "bad date: after" );
+        }
+
+        $sql = <<<EOT
+            SELECT a.id, a.srcid, a.title, c.content, a.pubdate, a.lastscraped, a.permalink,
+                    a.srcorg, o.shortname, o.prettyname, o.home_url
+                FROM ((article a INNER JOIN article_content c ON c.article_id=a.id) INNER JOIN organisation o ON o.id=a.srcorg)
+                WHERE a.lastscraped>?
+                ORDER BY a.lastscraped
+                LIMIT ?;
+EOT;
+        $r = db_query( $sql, $after_dt->format('Y-m-d\TH:i:s.uO'), $limit );
+    } elseif( $before ) {
+        $before_dt = date_create( $before );
+        if( !$before_dt ) {
+            throw new Exception( "bad date: before" );
+        }
+        $sql = <<<EOT
+            SELECT a.id, a.srcid, a.title, c.content, a.pubdate, a.lastscraped, a.permalink,
+                    a.srcorg, o.shortname, o.prettyname, o.home_url
+                FROM ((article a INNER JOIN article_content c ON c.article_id=a.id) INNER JOIN organisation o ON o.id=a.srcorg)
+                WHERE a.lastscraped<?
+                ORDER BY a.lastscraped DESC
+                LIMIT ?;
+EOT;
+        $r = db_query( $sql, $before_dt->format('Y-m-d\TH:i:s.uO'), $limit );
     }
 
-    $sql = <<<EOT
-        SELECT a.id, a.srcid, a.title, c.content, a.pubdate, a.lastscraped, a.permalink,
-                a.srcorg, o.shortname, o.prettyname, o.home_url
-            FROM ((article a INNER JOIN article_content c ON c.article_id=a.id) INNER JOIN organisation o ON o.id=a.srcorg)
-            WHERE a.lastscraped>?
-            ORDER BY a.lastscraped
-            LIMIT ?;
-EOT;
-    //$r = db_query( $sql, $after_dt->format(DateTime::ISO8601), $limit );
-    $r = db_query( $sql, $after_dt->format('Y-m-d\TH:i:s.uO'), $limit );
     $fields = array( 'id','srcid','title','content','pubdate','lastscraped','permalink' );
     $time_fields = array( 'pubdate','lastscraped' );
     while( $row = db_fetch_array( $r ) ) {
