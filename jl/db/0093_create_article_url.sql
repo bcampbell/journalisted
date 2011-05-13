4013e72d72d3b674869a949fc19ac2d4ebe64b69
bcampbell <ben@scumways.com>
2011-05-13 10:04:14 +0100

    attempt to speed up article_url transition

--- jl/db/0093_create_article_url.sql
+++ jl/db/0093_create_article_url.sql
@@ -4,8 +4,21 @@ BEGIN;
 -- select id,wordcount,permalink from article where permalink in (select permalink from article group by permalink having count(permalink)>1) order by permalink;
 
 -- plonk existing urls into article_url:
--- insert into article_url (SELECT permalink as url, id as article_id FROM article UNION SELECT srcurl as url, id as article_id FROM article);
+-- BEGIN;
+-- INSERT INTO article_url (url,article_id) (SELECT permalink AS url, id AS article_id FROM article UNION SELECT srcurl AS url, id AS article_id FROM article);
+-- COMMIT;
 
+
+
+
+
+BEGIN;
+-- use a temp table to move all permalink and srcurl values into the new article_url table
+CREATE TABLE tmp_article_urls ( url text NOT NULL, article_id integer );
+INSERT INTO tmp_article_urls (url,article_id) (SELECT permalink AS url, id AS article_id FROM article);
+INSERT INTO tmp_article_urls (url,article_id) (SELECT srcurl AS url, id AS article_id FROM article);
+-- index to speed up deduping
+-- CREATE INDEX tmp_article_urls_url_article_id_idx ON tmp_article_urls (url, article_id);
 
 CREATE TABLE article_url (
   id serial PRIMARY KEY,
@@ -13,7 +26,10 @@ CREATE TABLE article_url (
   article_id integer NOT NULL REFERENCES article(id) ON DELETE CASCADE
 );
 
-CREATE INDEX article_url_article_id_idx ON article_url (article_id);
+INSERT INTO article_url (url,article_id) (SELECT DISTINCT url,article_id FROM tmp_article_urls);
 
+CREATE INDEX article_url_article_id_idx ON article_url (article_id);
+CREATE INDEX article_url_url_idx ON article_url (url);
 
 COMMIT;
+
