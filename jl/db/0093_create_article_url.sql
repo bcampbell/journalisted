a3b838cdf14e5984abe48c27923149cd7112551b
bcampbell <ben@scumways.com>
2011-05-16 12:06:05 +0100

    streamline article_url addition

--- jl/db/0093_create_article_url.sql
+++ jl/db/0093_create_article_url.sql
@@ -9,27 +9,17 @@ BEGIN;
 -- COMMIT;
 
 
-
-
-
-BEGIN;
--- use a temp table to move all permalink and srcurl values into the new article_url table
-CREATE TABLE tmp_article_urls ( url text NOT NULL, article_id integer );
-INSERT INTO tmp_article_urls (url,article_id) (SELECT permalink AS url, id AS article_id FROM article);
-INSERT INTO tmp_article_urls (url,article_id) (SELECT srcurl AS url, id AS article_id FROM article);
--- index to speed up deduping
--- CREATE INDEX tmp_article_urls_url_article_id_idx ON tmp_article_urls (url, article_id);
-
 CREATE TABLE article_url (
   id serial PRIMARY KEY,
   url text NOT NULL,
   article_id integer NOT NULL REFERENCES article(id) ON DELETE CASCADE
 );
 
-INSERT INTO article_url (url,article_id) (SELECT DISTINCT url,article_id FROM tmp_article_urls);
+-- USE ../hacks/dump_article_urls to migrate over article URLS into new table
+-- (was too slow doing it all in DB). Might want to leave these indexes
+-- off until the data is loaded in.
 
 CREATE INDEX article_url_article_id_idx ON article_url (article_id);
 CREATE INDEX article_url_url_idx ON article_url (url);
-
 COMMIT;
 
