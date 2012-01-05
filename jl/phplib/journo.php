a4272d348d84d04583e4f2032758f44eed68b9b9
Ben Campbell <ben@scumways.com>
2012-01-05 12:37:39 +0000

    add test for new oneliner generation before deploy

--- jl/phplib/journo.php
+++ jl/phplib/journo.php
@@ -939,25 +939,49 @@ function journo_checkActivation( $journo_id )
 }
 
 
-function journo_buildOneliner($journo)
+function journo_buildOneliner($employers,$articles)
 {
-    $emps = db_getAll("SELECT kind,employer FROM journo_employment WHERE current=true AND journo_id=? ORDER BY year_from DESC", $journo['id']);
+    $names = array();
 
-    $employers = array();
-    foreach($emps as $emp) {
+    foreach($employers as $emp) {
+        if(!$emp['current'])
+            continue;
         $name = $emp['kind']=='f' ? 'Freelance' : $emp['employer'];
-        if(array_search(strtolower($name), array_map('strtolower', $employers)) === FALSE) {
-            $employers[] = $name;
-        }
-        if(sizeof($employers)>=3) {
-            break;
+        if(array_search(strtolower($name), array_map('strtolower', $names)) === FALSE) {
+            $names[] = $name;
         }
     }
 
-    if($employers) {
-        return implode(', ',$employers);
+    // clip to 3 employers, max
+    $names = array_slice($names,0,3);
+
+    if(!$names && sizeof($articles) >= 2) {
+        // nothing from employment data - guess using article history instead
+
+        $counts = array();
+        foreach($articles as $art) {
+            $pubname = $art['srcorgname'];
+            if(isset($counts[$pubname])) {
+                $counts[$pubname] += 1.0;
+            } else {
+                $counts[$pubname] = 1.0;
+            }
+        }
+        arsort($counts);
+
+        $total = (float)sizeof($articles);
+
+        $names = array();
+        foreach($counts as $pubname=>$cnt) {
+            if(($cnt/$total) > 0.2) {
+                $names[] = $pubname;
+            }
+        }
+
+        // clip to two publications, max
+        $names = array_slice($names,0,2);
     }
 
-    return '';
+    return implode(', ',$names);
 }
 
