22fd6234c35e47874b3f2781e9eee04f7add5531
Ben Campbell <ben@scumways.com>
2013-12-13 17:06:06 +1300

    ditch obsolete journo_scoring stuff

--- jl/phplib/journo.php
+++ jl/phplib/journo.php
@@ -655,30 +655,10 @@ EOT;
     $data['similar_journos'] = db_getAll( $sql, $journo['id'] );
 
 
-    /* collect journo score data */
-
-    $data['scoring'] = journo_collectScoring($journo);
-
     return $data;
 }
 
 
-
-function journo_collectScoring(&$journo) {
-    $out = array();
-
-    $out['num_admirers'] = (int)db_getOne("SELECT count(*) FROM journo_admired WHERE admired_id=?", $journo['id']);
-    $out['num_alerts'] = (int)db_getOne("SELECT count(*) FROM alert WHERE journo_id=?", $journo['id']);
-    return $out;
-
-
-    // TODO: stash scoring data in separate journo_score table, with daily update
-
-    // TODO: need to maintain max alerts and max admirers for weighting.
-    // select max(cnt) from (select admired_id,count(*) as cnt from journo_admired where admired_id is not null group by admired_id order by count(*)) as foo;
-    //
-    //select max(cnt) from (select count(*) as cnt from alert group by journo_id) as foo;
-}
 
 
 
