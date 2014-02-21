b8831f3447b970f2bdfe696a2951702f954076f8
Ben Campbell <ben@scumways.com>
2014-02-21 14:26:03 +1300

    front page hackery + some css cruft-removal
    New pared-down front page with only "recently changed profiles" and
    a signup form.

--- jl/templates/frontpage.tpl.php
+++ jl/templates/frontpage.tpl.php
@@ -1,107 +1,59 @@
 <?php
 
-// template for site front page
-//
-// params:
-//
-// $orgs - list of organisations the site covers
-//  for each entry
-//   shortname   - the internal name eg 'thesun'
-//   prettyname  - eg "The Sun"
-//
-// $news - list of recent newsletter entries
-//
-//
+    // template for site front page
+    //
+    // params:
+    //
+    //
 
 
 ?>
 <div class="main front-page">
 
-<div class="box front">
- <div class="head"></div>
- <div class="body">
-  <span class="brag">140,000 people used journa<i>listed</i> last month!</span>
-  <span class="youcan">On journa<i>listed</i> you can:</span>
-  <ul class="nav">
-   <li><a class="subscribe" href="/weeklydigest">Subscribe to the journa<i>listed</i> weekly digest</a></li>
-   <li><a class="edit-profile" href="/profile">edit profile</a></li>
-   <li><a class="search-articles" href="/search?type=article">search articles</a></li>
-   <li><a class="create-your-profile" href="/student">Student? Create your profile</a></li>
-   <li><a class="alerts" href="/alert">alerts</a></li>
-  </ul>
- </div>
- <div class="foot"></div>
-</div>
+    <div class="l-fullwidth">
+        <div class="box">
+            <div class="body">
 
-<div class="box recently-viewed">
-  <div class="head"><h3>Recently viewed</h3></div>
-  <div class="body">
+                <div class="fudge1" >Find journalists and see what they're writing about</div>
+               
+                <div class="row">
+                    <div class="col-5">
+                        <div class="box box-recently-updated">
+                            <div class="head"><h3>Recently updated profiles</h3></div>
+                            <div class="body">
 
-    <ul>
-<?php foreach( $recently_viewed as $j ) { ?>
-      <li><?= journo_link( $j ) ?></li>
-<?php } ?>
-    </ul>
-  </div>
-  <div class="foot"></div>
-</div>
+                                <ul>
+                                    <?php foreach( $recently_updated as $j ) { ?>
+                                    <li><?= journo_link($j) ?></li>
+                                    <?php } ?>
+                                </ul>
 
-<div class="box recently-updated">
-  <div class="head"><h3>Recently updated</h3></div>
-  <div class="body">
+                            </div>
+                            <div class="foot"></div>
+                        </div>  <!-- end .box-recently-updated -->
+                    </div>
+                    <div class="col-7">
+                        <div class="exhort">
+                            <h3>Are you a journalist? If so...<br/>
+                                Sign up or claim a profile - it's free</h3>
+                            <p class="fudge3">Creating a profile takes less than 3 minutes</p>
+                            <form>
+                                <input id="name" name="name" type="text" placeholder="name" value=""/>
+                                <input id="email" name="email" type="text" placeholder="email" value=""/>
+                                <input id="password" name="password" placeholder="password" type="password" value=""/>
+                                <input class="btn btn-lg" type="submit" value="Create/claim profile" />
+                            </form>
 
-    <ul>
-<?php foreach( $events as $e ) { ?>
-    <li><?= journo_link( $e['journo'] ) ?></li>
-<?php } ?>
-    </ul>
+                        </div>
 
-  </div>
-  <div class="foot"></div>
-</div>
 
-<div class="box most-blogged">
-  <div class="head"><h3>Most blogged about today</h3></div>
-  <div class="body">
-    <ul>
-<?php foreach( $most_blogged_about as $art ) { ?>
-        <li><a href="<?php echo article_url( $art['id'] ); ?>"><?php echo $art['title']; ?></a><br/>(<?php echo $art['total_bloglinks'];?> blogs)
-        </li>
-<?php } ?>
-    </ul>
-  </div>
-  <div class="foot"></div>
-</div>
-
-<div style="clear:both;"></div>
-
-<div class="box thisweek">
-<div class="head"><h3>journa<i>listed</i> weekly</h3></div>
-<div class="body">
- <ul>
-<?php foreach( $news as $n ) { ?>
-  <li>
-    <a href="/news/<?= $n['slug'] ?>"><?= $n['title'] ?></a>
-<?php if( $n['kind']=='newsletter') { ?>
-      <small>(for week ending <?= $n['pretty_to'] ?>)</small>
-<?php } ?>
-  </li>
-<?php } ?>
- </ul>
- <a href="/news">more...</a>
-</div>
-<div class="foot"></div>
-</div>
-
-<div class="box tobias">
-<div class="head"></div>
-<div class="body">
-<img src="/img/tobias.png" alt="Tobias Grubbe" />
-<p>Read the opinions of <a href="/tobias-grubbe">Tobias Grubbe</a> at journa<i>listed</i>.com</p>
-<div style="clear:both;"></div>
-</div>
-<div class="foot"></div>
-</div>
+                    </div>
+                </div> <!-- end row -->
+                <br/>
+                <br/>
+            </div>
+        </div>
+    </div>
 
 </div>  <!-- end main -->
 <?php
--- jl/web/index.php
+++ jl/web/index.php
@@ -15,7 +15,6 @@ require_once '../../phplib/db.php';
 require_once '../phplib/journo.php';
 require_once '../phplib/article.php';
 
-NEW_version();
 
 
 function extra_head()
@@ -43,7 +42,7 @@ function extra_head()
 
 
 
-function NEW_version()
+function view()
 {
 
 
@@ -59,20 +58,8 @@ EOT;
 
     page_header( "", array( 'menupage'=>'cover', 'head_extra_fn'=>"extra_head" ) );
 
-/*
-    $sql = <<<EOT
-SELECT j.ref AS journo_ref, j.prettyname as journo_prettyname, e.event_time, e.event_type, e.context_json
-    FROM event_log e LEFT JOIN journo j ON j.id=e.journo_id
-    WHERE event_time>NOW()-interval '12 hours'
-    ORDER BY event_time DESC
-    LIMIT 10;
-EOT;
-    $events = db_getAll( $sql );
-    foreach( $events as &$ev ) {
-//        $ev['context'] = json_decode( $ev['context_json'], TRUE );
-        $ev['description'] = eventlog_Describe( $ev );
-    }
-*/
+    // get most-recently-updated journos from event log
+    $recently_updated  = array();
 
     $sql = <<<EOT
 SELECT j.ref AS journo_ref, j.prettyname as journo_prettyname, j.oneliner as journo_oneliner, min(now()-e.event_time) as when
@@ -82,48 +69,13 @@ SELECT j.ref AS journo_ref, j.prettyname as journo_prettyname, j.oneliner as jou
     ORDER BY min( now()-e.event_time) ASC
     LIMIT 10;
 EOT;
+
     $events = db_getAll( $sql );
     foreach( $events as &$ev ) {
-//        $ev['context'] = json_decode( $ev['context_json'], TRUE );
-        $ev['description'] = $ev['when'];    //eventlog_Describe( $ev );
-        $ev['journo'] = array( 'ref'=> $ev['journo_ref'], 'prettyname'=>$ev['journo_prettyname'], 'oneliner'=>$ev['journo_oneliner'] );
-        unset( $ev['journo_ref'] );
-        unset( $ev['journo_prettyname'] );
-        unset( $ev['journo_oneliner'] );
+        $recently_updated[] = array( 'ref'=> $ev['journo_ref'], 'prettyname'=>$ev['journo_prettyname'], 'oneliner'=>$ev['journo_oneliner'] );
     }
 
-    // recent newsletters
-    $news = news_RecentNews(4);
-
-    $orgs = db_getAll( "SELECT shortname,prettyname FROM organisation ORDER BY prettyname" );
-
-
-    // most blogged-about articles
-    $sql = <<<EOT
-SELECT a.id,a.srcorg,a.title,a.permalink,a.total_bloglinks,o.prettyname as srcorgname
-    FROM article a INNER JOIN organisation o ON a.srcorg=o.id
-    WHERE a.pubdate <= NOW() AND a.pubdate > NOW()-interval '48 hours'
-        AND a.total_bloglinks > 0
-    ORDER BY a.total_bloglinks DESC
-    LIMIT 5
-EOT;
-
-    $most_blogged_about = db_getAll( $sql );
-    foreach( $most_blogged_about as &$a ) {
-        article_addJournos( $a );
-    }
-    unset( $a );
-
-    // recently-viewed journos
-    // TODO: really not so happy about this... (see web/journo.php too)
-    $sql = <<<EOT
-SELECT j.ref, j.prettyname, j.oneliner
-    FROM recently_viewed v INNER JOIN journo j ON j.id=v.journo_id
-    ORDER BY v.view_time DESC
-    LIMIT 10
-EOT;
-    $recently_viewed = db_getAll( $sql );
-
+    // invoke the template
     {
         include "../templates/frontpage.tpl.php";
     }
@@ -132,201 +84,6 @@ EOT;
 }
 
 
-function article_addJournos( &$a )
-{
-    $j = db_getAll( "SELECT prettyname,ref FROM journo j INNER JOIN journo_attr attr ON attr.journo_id=j.id WHERE attr.article_id=? LIMIT 1", ($a['id']) );
-    if( $j )
-        $a['journos'] = $j;
-    else
-        $a['journos'] = array();
-}
-
-
-function OLD_version()
-{
-
-$orgs = db_getAll( "SELECT shortname,prettyname FROM organisation ORDER BY prettyname" );
-
-// setup for placeholder text in input forms
-$head_extra = <<<EOT
-  <script type="text/javascript" language="JavaScript">
-    window.onload=function() {
-      activatePlaceholders();
-    }
-  </script>
-EOT;
-
-page_header( "", array( 'menupage'=>'cover', 'head_extra'=>$head_extra ) );
-
-?>
-
-<div class="greenbox">
-Journa<i>listed</i> is an independent, non-profit web service to help the public navigate the news
-</div>
-
-<div id="maincolumn">
-
-
-
-
-
-<div class="action-box">
- <div class="action-box_top"><div></div></div>
-  <div class="action-box_content">
-
-   <form action="/list" method="get" class="frontform">
-    <label for="name">Find out more about a journalist</label>
-    <input type="text" value="" title="type journalist name here" id="name" name="name" placeholder="type journalist name here" class="text" />
-<!--    <input type="submit" value="Find" /><br /> -->
-    <input type="image" src="images/white_arrow.png" alt="find" />
-   </form>
-
-   <form action="/list" method="get" class="frontform">
-    <label for="outlet">Track down a journalist by news outlet</label>
-
-     <select id="outlet" name="outlet" class="select" >
-<?php
-	foreach( $orgs as $o )
-		print "   <option value=\"{$o['shortname']}\">{$o['prettyname']}</option>\n";
-?>
-     </select>
-    <input type="image" src="images/white_arrow.png" alt="find" />
-<!--    <input type="submit" value="Find" /><br /> -->
-   </form>
-
-
-   <form action="/search" method="get" class="frontform">
-    <label for="q">Search articles</label>
-    <input type="text" value="" title="type subject here" id="q" name="q" class="text" placeholder="type subject here"/>
-<!--    <input type="submit" value="Find" /><br /> -->
-    <input type="image" src="images/white_arrow.png" alt="find" />
-   </form>
-
-  </div>
- <div class="action-box_bottom"><div></div></div>
-</div>
-
-
-
-
-<?php
-#$featured = db_getRow( "SELECT * FROM journo WHERE ref='jeff-prestridge'" );
-#$featured = db_getRow( "SELECT * FROM journo WHERE ref='ben-goldacre'" );
-$featured = db_getRow( "SELECT * FROM journo WHERE ref='jim-alkhalili'" );
-frontpage_emitFeaturedJourno( $featured );
-cache_emit( "fp_tags","emit_whoswritingaboutwhat",60*60*24 );
-?>
-
-</div>  <!-- end maincolumn -->
-
-
-<div id="smallcolumn">
-<div class="greenbox">Build your own newsroom</div>
-<div class="greenbox">Donate</div>
-<div class="greenbox">journa<i>listed</i> blog</div>
-<?php emit_mostbloggedarticles(); ?>
-<?php emit_mostcommentedarticles(); ?>
-<div class="greenbox">What should Journa<i>listed</i> do next?</div>
-</div>
-
-<?php
-
-page_footer();
-}
-
-
-
-function emit_my_journos_box( &$P )
-{
-?>
- <div class="boxnarrow myjournos">
-  <h2>My Journalisted</h2>
-  <div class="boxnarrow-content">
-<?php
-
-    $P = person_if_signed_on(true); /* Don't renew any login cookie. */
-	if( $P )	
-	$q = db_query( "SELECT a.id,a.journo_id, j.prettyname, j.ref " .
-		"FROM (alert a INNER JOIN journo j ON j.id=a.journo_id) " .
-		"WHERE a.person_id=? ORDER BY j.lastname", $P->id );
-	print "  <ul>\n";
-	while( $row=db_fetch_array($q) )
-	{
-		$journourl = "/{$row['ref']}";
-		printf( "   <li><a href=\"%s\">%s</a></li>\n", $journourl, $row['prettyname'] );
-	}
-	print( "  </ul>\n" );
-
-?>
-  </div>
- </div>
-<?php
-
-
-}
-
-
-// a list of the journos who were most recently published.
-// Well, OK, most recently _scraped_ :-)
-// It'd be nicer to use time of publication (pubdate field),
-// but a lot of outlets only provide a date, not a time.
-// ("firstseen" datetime would be more appropriate, but it's usually
-// the same for all articles in each scraper run, which makes for a
-// boring list).
-function emit_recent_journos_box()
-{
-?>
- <div class="boxnarrow recentjournos">
- <h2>Recently Published</h2>
- <div class="boxnarrow-content">
-
-<?php
-
-	// want 10, but get 20 of them, just in case some have written multiple articles...
-	$sql = <<<EOT
-SELECT j.prettyname,j.ref
-	FROM ( (article a INNER JOIN journo_attr attr ON attr.article_id=a.id)
-		INNER JOIN journo j ON attr.journo_id=j.id )
-	WHERE a.status='a'
-		AND j.status='a'
---		AND a.pubdate > NOW()-interval '1 day'
-		AND a.pubdate < NOW()
-	ORDER BY a.pubdate desc
-	LIMIT 20;
-EOT;
-
-	$q = db_query( $sql );
-	/* don't want dupes */
-	$uniq = array();
-	$maxage = 0;
-	while( $row=db_fetch_array($q) )
-	{
-		$journourl = "/{$row['ref']}";
-		$uniq[ $row['prettyname'] ] = array( 'url'=>$journourl );
-	}
-
-?>
-  <p>The most recently published Journalists are:</p>
-  <ul>
-<?php
-
-	/* output the list */
-	$cnt = 0;
-	foreach( $uniq as $name=>$info )
-	{
-		++$cnt;
-		if( $cnt >= 10 )
-			break;
-/*		printf( "   <li><a href=\"%s\">%s</a> <small>(%s ago)</small></li>\n", $info['url'], $name, $info['age'] ); */
-		printf( "   <li><a href=\"%s\">%s</a></li>\n", $info['url'], $name );
-	}
-
-?>
-  </ul>
- </div>
- </div>
-
-<?php
-}
+view();
 
 ?>
