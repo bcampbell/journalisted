9af05e9737212c40f0733a4faa1c42a860634208
Ben Campbell <ben@scumways.com>
2016-06-10 15:56:44 +1200

    sun: new website
    A proper website, this time, not the silly javascript muppetry
    of the old one.

--- jl/scraper/sun.py
+++ jl/scraper/sun.py
@@ -4,18 +4,9 @@
 # Licensed under the Affero General Public License
 # (http://www.affero.org/oagpl.html)
 #
-# NOTE: The sun isn't a real web site - it's a (more-or-less)
-# single page app which uses javascript to fetch and display
-# articles.
 
 import re
-import urllib   # for urlencode
-import urllib2
-import ConfigParser
-import sys
-import traceback
 from datetime import date,datetime
-import urlparse
 import lxml.html
 
 import site
@@ -23,115 +14,64 @@ site.addsitedir("../pylib")
 from BeautifulSoup import BeautifulSoup
 from JL import ukmedia,ScraperUtils
 
-from pprint import pprint
-
-try:
-    import json
-except ImportError:
-    import simplejson as json
-
 
 base_url = 'http://www.thesun.co.uk'
 
 
-def jsonify_url(url):
-    """ convert a sun html url to it's webapp equivalent
+def FindArticles(sesh):
+    """ get current active articles by scanning each section page """
+    urls = ScraperUtils.GenericFindArtLinks(
+            start_page = base_url,
+            domain_whitelist = "www.thesun.co.uk",
+            nav_sel = "#sun-menu a, .sub-nav__container a",
+            art_url_pat = re.compile(r'/\d{5,}/[^/]+/?$'),
+            article_blacklist = ["/football/team/",]
+        )
 
-    The sun isn't a real website - all the pages are just
-    stubs which fetch json and display it via javascript.
-    All article and section URLs have equivalent json URLs.
-    """
+    return [ ContextFromURL(u) for u in urls ]
 
-    return url.replace("thesun.co.uk/sol/", "thesun.co.uk/web/thesun/sol/")
 
 def ContextFromURL( url ):
     """get a context ready for scraping a single url"""
     context = {}
     context['permalink'] = url
     context['srcorgname'] = u'sun'
-    # Fudge the srcurl to grab the json data
-    context['srcurl'] = jsonify_url(url)
+    context['srcurl'] = url
     context['lastseen'] = datetime.now()
     return context
 
 
 def Extract( html, context, **kw ):
     art = context
-    # not really html - it's json
-    dat = json.loads(html)
+    parser = lxml.html.HTMLParser(encoding='utf-8')
+    doc = lxml.html.document_fromstring(html, parser, base_url=art['srcurl'])
 
-    art['title'] = ukmedia.FromHTMLOneLine(dat['headline'])
-    art['content'] = dat['articleBody']
-    unix_time = dat['articlePublishedTimestamp']/1000
-    art['pubdate'] = datetime.fromtimestamp(unix_time)
-    if 'authorByline' in dat:
-        art['byline'] = ukmedia.FromHTMLOneLine(dat['authorByline']['byline'])
-    else:
-        art['byline'] = u''
+    article = doc.cssselect('article.article')[0]
+    h1 = doc.cssselect('header .article__headline')[0]
+    art['title'] = ukmedia.FromHTMLOneLine(unicode(lxml.html.tostring(h1)))
+
+    art['byline'] = u''
+    authors = doc.cssselect('.article__meta .article__author-name')
+    if len(authors)>0:
+        parts = [ukmedia.FromHTMLOneLine(a.text_content()) for a in authors]
+        art['byline'] = u', '.join(parts)
+
+
+    pubdates = doc.cssselect('.article__meta .article__published')
+    if len(pubdates)>0:
+        art['pubdate'] = ukmedia.ParseDateTime(pubdates[0].text_content())
+
+    body_div = article.cssselect('.article__content')[0]
+
+    # cruft removal
+    for cruft in body_div.cssselect('.rail-video-index'):
+        cruft.drop_tree()
+
+    art['content'] = ukmedia.SanitiseHTML(unicode(lxml.html.tostring(body_div)))
+    art['description'] = ukmedia.FirstPara( art['content'] )
+    art['srcorgname'] = u'sun'
 
     return art
-
-def FindArticles(sesh):
-
-    arts = set()
-    section_urls = find_sections(sesh) 
-    for sect_url in section_urls:
-        raw = ukmedia.FetchURL(jsonify_url(sect_url), sesh=sesh)
-        dat = json.loads(raw)
-
-        for teaser in dat['articleTeasers']:
-
-            url = teaser['articleUrl']
-            if teaser['articleType'] != u'article':
-#                ukmedia.DBUG("WARN NONARTICLE: %s\n" %(url,))
-                continue
-
-            if not url.startswith('/'):
-#                ukmedia.DBUG("WARN FOOK1: %s\n" %(url,))
-                continue
-            # articleType 'article'
-            url = base_url + url
-            arts.add(url)
-
-    return [ ContextFromURL(u) for u in arts ]
-
-
-
-def find_sections(sesh):
-
-    menu_url = base_url + '/web/thesun/sol/resources/menu/all/471/'
-    raw = ukmedia.FetchURL(menu_url, sesh=sesh)
-    sects = json.loads(raw)
-    #pprint( sects)
-
-    urls = set()
-    for foo in sects:
-        for item in foo['items']:
-            if 'url' not in item:
-                continue
-            url = item['url']
-            if not url.startswith('/'):
-                continue
-            if u'.html' in url:
-                continue
-            if u'.ece' in url:
-                continue
-            if u'/page3/' in url:
-                continue
-            if u'/video/' in url:
-                continue
-
-            url = base_url + url
-            urls.add(url)
-
-    return urls
-
-
-
-
-    raw = ukmedia.FetchURL(url, sesh=sesh)
-    raw = ukmedia.FetchURL(url, sesh=sesh)
-    return json.loads(raw)
 
 
 
